b36091ffb7ece5e528bf276bbced6713901f28f4
Sinisa <sinisa.neskovic@fon.bg.ac.rs>
2020-03-09 14:30:36 +0100

    fixed rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'index' // 'aelastics-types'
+const libraryName = 'aelastics-types' // 'aelastics-types'
 
 export default {
   input : `src/${libraryName}.ts` ,
