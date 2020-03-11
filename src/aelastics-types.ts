d70763923b92078f4b3f3466bc9e82a80f29281f
Sinisa <sinisa.neskovic@fon.bg.ac.rs>
2020-03-11 02:31:51 +0100

    Added schema and links, fixed other types

--- src/aelastics-types.ts
+++ src/aelastics-types.ts
@@ -3,6 +3,8 @@
 // ...
 // import { TypeC } from './common/Type'
 
+import { Any } from './common/Type'
+
 export { Type, Any, TypeOf, DtoTypeOf } from './common/Type'
 export { boolean } from './simple-types/Boolean'
 export { date } from './simple-types/DateType'
@@ -42,6 +44,9 @@ import * as stn from './simple-types/Number'
 import * as stl from './simple-types/Literal'
 import * as std from './simple-types/DateType'
 
+import * as link from './common/LinkC'
+import * as schema from './common/TypeSchema'
+
 export namespace types {
   export type Type<T, D = T> = ct.TypeC<T, D>
   export type Any = ct.Any
@@ -50,10 +55,10 @@ export namespace types {
   export type DtoObjectType<T extends cot.Props> = cot.DtoObjectType<T>
 
   export type ArrayType<T extends ct.Any> = cat.ArrayTypeC<T>
-  export type ComplexType<T extends ct.Any> = cct.ComplexTypeC<T, any>
+  export type ComplexType<P, T extends any, D extends any = T> = cct.ComplexTypeC<T, D>
   export type FunctionalType<T extends cot.Props> = cft.FunctionalTypeC<T, any>
   export type IntersectionType<P extends Array<Any>> = cit.IntersectionTypeC<P>
-  export type MapType<T extends ct.Any> = cmt.MapTypeC<T, any>
+  export type MapType<K extends Any, V extends Any> = cmt.MapTypeC<K, V>
   export type ObjReference<T extends cot.ObjectTypeC<any>> = cort.ObjReference<T>
   export type Subtype<
     P extends cot.Props,
@@ -68,9 +73,12 @@ export namespace types {
 
   export type SimpleType<T> = st.SimpleTypeC<T>
   export type OptionalType<T extends Any> = sto.OptionalTypeC<T>
-  export type String = sts.StringTypeC
+  export type StringType = sts.StringTypeC
   export type BooleanType = stb.BooleanTypeC
   export type NumberType = stn.NumberTypeC
   export type LiteralType<T extends stl.LiteralValue> = stl.LiteralTypeC<T>
   export type DateType = std.DateTypeC
+
+  export type TypeSchema = schema.TypeSchema
+  export type LinkType = link.LinkC
 }
