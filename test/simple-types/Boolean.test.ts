a302ab60dc3652816eb72ed45eb7480128d99fe1
nikolakojic997 <volimdahodam@gmail.com>
2020-04-01 21:00:24 +0200

    Fixed validate calls in all simple type testes

--- test/simple-types/Boolean.test.ts
+++ test/simple-types/Boolean.test.ts
@@ -6,13 +6,13 @@ describe('Test cases for boolean type', () => {
   it("should verify that 'true' is boolean.", () => {
     const realBoolean = t.boolean
     let value = true
-    expect(isSuccess(realBoolean.validate(value, []))).toBe(true)
+    expect(isSuccess(realBoolean.validate(value))).toBe(true)
   })
 
   it("should verify that 'false' is boolean.", () => {
     const realBoolean = t.boolean
     let value = false
-    expect(isSuccess(realBoolean.validate(value, []))).toBe(true)
+    expect(isSuccess(realBoolean.validate(value))).toBe(true)
   })
 
   // compiler doesn't let us run this tests
@@ -20,24 +20,24 @@ describe('Test cases for boolean type', () => {
   it("should verify that 'null' is not boolean.", () => {
     const realBoolean = t.boolean
     let value = null
-    expect(isSuccess(realBoolean.validate((value as unknown) as boolean, []))).toBe(false)
+    expect(isSuccess(realBoolean.validate((value as unknown) as boolean))).toBe(false)
   })
 
   it("should verify that 'undefined' is not boolean.", () => {
     const realBoolean = t.boolean
     let value = undefined
-    expect(isSuccess(realBoolean.validate((value as unknown) as boolean, []))).toBe(false)
+    expect(isSuccess(realBoolean.validate((value as unknown) as boolean))).toBe(false)
   })
 
   it('should verify that Number type value is not boolean.', () => {
     const realBoolean = t.boolean
     let value = 1
-    expect(isSuccess(realBoolean.validate((value as unknown) as boolean, []))).toBe(false)
+    expect(isSuccess(realBoolean.validate((value as unknown) as boolean))).toBe(false)
   })
 
   it('should verify that String type is not boolean.', () => {
     const realBoolean = t.boolean
     let value = 'abs'
-    expect(isSuccess(realBoolean.validate((value as unknown) as boolean, []))).toBe(false)
+    expect(isSuccess(realBoolean.validate((value as unknown) as boolean))).toBe(false)
   })
 })
