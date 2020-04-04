fc00ceda5d18be6be9b44cea8e7a6cf0d1f5fc7f
ivanatodorovic <ititodorovic1998@gmail.com>
2020-04-04 17:57:27 +0200

    Changed fromDTO test cases for complex graph type Union

--- test/complex-types-graph/FromDTO-tests/Union-fromDTO.test.ts
+++ test/complex-types-graph/FromDTO-tests/Union-fromDTO.test.ts
@@ -1,31 +1,53 @@
 import * as examples from '../testing-types'
 import { isFailure, isSuccess } from 'aelastics-result'
+import * as t from '../../../src/aelastics-types'
 
 describe('fromDTO tests for Union type', () => {
   it('Testing fromDTO with basic case, should be true', () => {
-    let pDTO = {
-      name: 'Nick',
-      title: 'Msc'
+    let pDTO: t.DtoTypeOf<typeof examples.EmployeeUnionType> = {
+      ref: { id: 1, category: 'Union', typeName: 'employee' },
+      typeInUnion: 'profesor',
+      union: {
+        ref: { id: 2, category: 'Object', typeName: 'profesor' },
+        object: {
+          name: 'Nick',
+          title: 'Msc'
+        }
+      }
     }
-    let p = examples.EmployeeUnionType.fromDTO(pDTO, [])
+    let p = examples.EmployeeUnionType.fromDTO(pDTO)
     expect(isSuccess(p)).toBe(true)
   })
 
-  it('Testing fromDTO for EmployeeUnionType in case of wrong field name, should be false ', () => {
-    let pDTO = {
-      name: 'Nick',
-      surname: 'Msc'
+  it('Testing fromDTO for EmployeeUnionType for wrong property value, should be false', () => {
+    let pDTO: t.DtoTypeOf<typeof examples.EmployeeUnionType> = {
+      ref: { id: 1, category: 'Union', typeName: 'employee' },
+      typeInUnion: 'janitor',
+      union: {
+        ref: { id: 2, category: 'Object', typeName: 'janitor' },
+        object: {
+          name: 'Nick',
+          age: -15
+        }
+      }
     }
-    let p = examples.EmployeeUnionType.fromDTO((pDTO as unknown) as any, [])
+    let p = examples.EmployeeUnionType.fromDTO((pDTO as unknown) as any)
     expect(isSuccess(p)).toBe(false)
   })
 
   it('Testing fromDTO message for EmployeeUnionType in case of unsatisfied constraint for fields', () => {
-    let pDTO = {
-      name: 'Nick11',
-      title: 'Msc'
+    let pDTO: t.DtoTypeOf<typeof examples.EmployeeUnionType> = {
+      ref: { id: 1, category: 'Union', typeName: 'employee' },
+      typeInUnion: 'profesor',
+      union: {
+        ref: { id: 2, category: 'Object', typeName: 'profesor' },
+        object: {
+          name: 'Nick11',
+          title: 'Msc'
+        }
+      }
     }
-    let p = examples.EmployeeUnionType.fromDTO(pDTO, [])
+    let p = examples.EmployeeUnionType.fromDTO(pDTO)
     if (isFailure(p)) {
       expect(examples.errorMessages(p)).toBe("Value : '[object Object]' is not union: 'employee'\n")
     }
@@ -33,21 +55,33 @@ describe('fromDTO tests for Union type', () => {
 
   it('Testing fromDTO for EmployeeUnionType with extra fields, should be true', () => {
     let pDTO = {
-      name: 'Nick',
-      title: 'Msc',
-      surname: 'Scott'
+      ref: { id: 1, category: 'Union', typeName: 'employee' },
+      typeInUnion: 'profesor',
+      union: {
+        ref: { id: 2, category: 'Object', typeName: 'profesor' },
+        object: {
+          name: 'Nick',
+          title: 'Msc',
+          surname: 'Scott'
+        }
+      }
     }
-    let p = examples.EmployeeUnionType.fromDTO(pDTO, [])
+    let p = examples.EmployeeUnionType.fromDTO(pDTO)
     expect(isSuccess(p)).toBe(true)
   })
 
   it('Testing fromDTO with literal, should be true', () => {
-    let g = examples.gradeType.fromDTO('failed', [])
+    // let DtoType: t.DtoTypeOf<typeof examples.gradeType> = {
+    //   ref:{id:1, category:'Union', typeName:'grade'},
+    //   typeInUnion:'',
+    //   union:{}
+    //   };
+    let g = examples.gradeType.fromDTO('failed')
     expect(isSuccess(g)).toBe(true)
   })
 
   it('Testing fromDTO message for gradeType in case of unsatisfied constraint for fields', () => {
-    let g = examples.gradeType.fromDTO(11, [])
+    let g = examples.gradeType.fromDTO(11)
     if (isFailure(g)) {
       expect(examples.errorMessages(g)).toBe("Value : '11' is not union: 'grade'\n")
     }
