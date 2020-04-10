68d6393646aad70572d1a500016c2a865761d53f
Sinisa <sinisa.neskovic@fon.bg.ac.rs>
2020-04-10 11:31:08 +0200

    fixed more tests

--- test/complex-types-tree/ToDTO-tests/IntersectionType-toDTO.test.ts
+++ test/complex-types-tree/ToDTO-tests/IntersectionType-toDTO.test.ts
@@ -1,53 +1,53 @@
-// import { isSuccess, isFailure } from 'aelastics-result'
-// import { errorMessages, ProfessorIntersectionType } from '../testing-types'
-// import { FullNameType } from '../../example/types-example'
-//
-// describe('toDTO test cases for IntersectionType', () => {
-//   it('should be valid toDTO for FullNameType', () => {
-//     const fullName = { name: 'John', familyName: 'Brown' }
-//     const res = FullNameType.toDTO(fullName, [])
-//     expect(isSuccess(res)).toBe(true)
-//   })
-//
-//   it('should be valid toDTO value for FullNameType', () => {
-//     const fullName = { name: 'John', familyName: 'Brown' }
-//     const res = FullNameType.toDTO(fullName, [])
-//     if (isSuccess(res)) {
-//       expect(res.value).toEqual({ familyName: 'Brown', name: 'John' })
-//     }
-//   })
-//
-//   it('should not be valid toDTO for FullNameType', () => {
-//     const fullName = { name: 2, familyName: 'Brown' }
-//     const res = FullNameType.toDTO((fullName as unknown) as any, [])
-//     expect(isSuccess(res)).toBe(false)
-//   })
-//
-//   it('should be valid error message toDTO for FullNameType', () => {
-//     const fullName = { name: 2, familyName: 'Brown' }
-//     const res = FullNameType.toDTO((fullName as unknown) as any, [])
-//     if (isFailure(res)) {
-//       expect(errorMessages(res)).toEqual('Expected name:2 to be alphabetical, got `2`\n')
-//     }
-//   })
-//
-//   it('should not be valid toDTO for FullNameType in case map with fields', () => {
-//     const fullName = new Map()
-//     fullName.set('name', 'John')
-//     fullName.set('familyName', 'Brown')
-//     const res = FullNameType.toDTO((fullName as unknown) as any, [])
-//     expect(isSuccess(res)).toBe(false)
-//   })
-//
-//   it('should be valid ProfessorIntersectionType', () => {
-//     const professor = { name: 'John', age: 45, title: 'Msc' }
-//     const res = ProfessorIntersectionType.toDTO(professor, [])
-//     expect(isSuccess(res)).toBe(true)
-//   })
-//
-//   it('should not be valid ProfessorIntersectionType in case of unsatisfied constraints', () => {
-//     const professor = { name: 'John223', age: 45, title: 'Msc' }
-//     const res = ProfessorIntersectionType.toDTO(professor, [])
-//     expect(isSuccess(res)).toBe(false)
-//   })
-// })
+import { isSuccess, isFailure } from 'aelastics-result'
+import { errorMessages, ProfessorIntersectionType } from '../testing-types'
+import { FullNameType } from '../../example/types-example'
+
+describe('toDTO test cases for IntersectionType', () => {
+  it('should be valid toDTO for FullNameType', () => {
+    const fullName = { name: 'John', familyName: 'Brown' }
+    const res = FullNameType.toDTO(fullName)
+    expect(isSuccess(res)).toBe(true)
+  })
+
+  it('should be valid toDTO value for FullNameType', () => {
+    const fullName = { name: 'John', familyName: 'Brown' }
+    const res = FullNameType.toDTO(fullName)
+    if (isSuccess(res)) {
+      expect(res.value).toEqual({ familyName: 'Brown', name: 'John' })
+    }
+  })
+
+  it('should not be valid toDTO for FullNameType', () => {
+    const fullName = { name: 2, familyName: 'Brown' }
+    const res = FullNameType.toDTO((fullName as unknown) as any)
+    expect(isSuccess(res)).toBe(false)
+  })
+
+  it('should be valid error message toDTO for FullNameType', () => {
+    const fullName = { name: 2, familyName: 'Brown' }
+    const res = FullNameType.toDTO((fullName as unknown) as any)
+    if (isFailure(res)) {
+      expect(errorMessages(res)).toEqual('Expected name:2 to be alphabetical, got `2`\n')
+    }
+  })
+
+  it('should not be valid toDTO for FullNameType in case map with fields', () => {
+    const fullName = new Map()
+    fullName.set('name', 'John')
+    fullName.set('familyName', 'Brown')
+    const res = FullNameType.toDTO((fullName as unknown) as any)
+    expect(isSuccess(res)).toBe(false)
+  })
+
+  it('should be valid ProfessorIntersectionType', () => {
+    const professor = { name: 'John', age: 45, title: 'Msc' }
+    const res = ProfessorIntersectionType.toDTO(professor)
+    expect(isSuccess(res)).toBe(true)
+  })
+
+  it('should not be valid ProfessorIntersectionType in case of unsatisfied constraints', () => {
+    const professor = { name: 'John223', age: 45, title: 'Msc' }
+    const res = ProfessorIntersectionType.toDTO(professor)
+    expect(isSuccess(res)).toBe(false)
+  })
+})
