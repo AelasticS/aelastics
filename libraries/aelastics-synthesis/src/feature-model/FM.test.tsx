b28d3d99db308e318ea442b3ffd5241313f9dac4
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2022-11-20 22:18:53 +0100

    add FM metamodel types

---
+++ libraries/aelastics-synthesis/src/feature-model/FM.test.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+import { STX } from "../jsx/handle";
+import * as fm from "./fm-meta.model.type";
+
+// let testFM = new fm.FeatureDiagram();
+
+describe("First test of FM metamodel", () => {
+  test("test1", () => {
+    console.log("proba testa");
+    expect(true).toBeTruthy;
+  });
+});
--- libraries/aelastics-synthesis/src/feature-model/fm-meta.model.type.ts
+++ libraries/aelastics-synthesis/src/feature-model/fm-meta.model.type.ts
@@ -6,27 +6,76 @@
 import * as t from "aelastics-types";
 import { ModelElement, Model } from "generic-metamodel";
 
-export const FMModel_TypeSchema = t.schema("FMModel");
+export const FMModel_TypeSchema = t.schema("FMModelSchema");
 
-export const FeatureModel = t.subtype(
+export const FMConcept = t.subtype(
+  ModelElement,
+  {},
+  "FMConcept",
+  FMModel_TypeSchema
+);
+
+export const FeatureDiagram = t.subtype(
   Model,
   {
-    aaa: t.string,
-    child: t.link(FMModel_TypeSchema, "Feature"),
+    root: t.link(FMModel_TypeSchema, "Root"),
   },
-  "FeatureModel",
+  "FeatureDiagram",
+  FMModel_TypeSchema
+);
+
+export const Attribute = t.subtype(
+  FMConcept,
+  {
+    name: t.string,
+  },
+  "Attribute",
   FMModel_TypeSchema
 );
 
 export const Feature = t.subtype(
-  ModelElement,
+  FMConcept,
   {
-    parent: FeatureModel,
+    minCardinality: t.string.derive().oneOf(["0", "1", "M", "m", "*"]),
+    maxCardinality: t.string.derive().oneOf(["1", "M", "m", "*"]),
+    attributes: t.arrayOf(Attribute),
   },
   "Feature",
   FMModel_TypeSchema
 );
 
-t.inverseProps(Feature, "parent", FeatureModel, "child");
+export const Root = t.subtype(Feature, {}, "Root", FMModel_TypeSchema);
+export const Child = t.subtype(Feature, {}, "Child", FMModel_TypeSchema);
 
-// t.unionOf([t.literal('male'), t.literal("female")],"sexType"),
+export const GroupElement = t.subtype(
+  Feature,
+  {},
+  "GroupElement",
+  FMModel_TypeSchema
+);
+
+export const SolitaryFeature = t.subtype(
+  Child,
+  {},
+  "SolitaryFeature",
+  FMModel_TypeSchema
+);
+
+export const GroupFeature = t.subtype(
+  Child,
+  {
+    elements: t.arrayOf(GroupElement),
+  },
+  "GroupFeature",
+  FMModel_TypeSchema
+);
+
+export type IFMConcept = t.TypeOf<typeof FMConcept>;
+export type IAttribute = t.TypeOf<typeof Attribute>;
+export type IFeatureDiagram = t.TypeOf<typeof FeatureDiagram>;
+export type IFeature = t.TypeOf<typeof Feature>;
+export type Root = t.TypeOf<typeof Root>;
+export type Child = t.TypeOf<typeof Child>;
+export type GroupElement = t.TypeOf<typeof GroupElement>;
+export type SolitaryFeature = t.TypeOf<typeof SolitaryFeature>;
+export type GroupFeature = t.TypeOf<typeof GroupFeature>;
