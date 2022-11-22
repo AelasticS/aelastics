3f882ffbe7b54d78862960e26246e7fff5ade51a
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2022-11-22 22:52:52 +0100

    ispravi FM metamodel. dodaj primer instance FM mm

--- libraries/aelastics-synthesis/src/feature-model/FM.test.tsx
+++ libraries/aelastics-synthesis/src/feature-model/FM.test.tsx
@@ -2,13 +2,74 @@
  *
  */
 
-import { STX } from "../jsx/handle";
 import * as fm from "./fm-meta.model.type";
 
 // let testFM = new fm.FeatureDiagram();
 
 describe("First test of FM metamodel", () => {
   test("test1", () => {
+    let r: fm.IRoot = {
+      attributes: [],
+      name: "feature root ",
+      minCardinality: "1",
+      maxCardinality: "1",
+    };
+
+    let a1: fm.IAttribute = {
+      name: "attr1",
+      type: "string", // namerno napravljena greska
+    };
+
+    let f1: fm.ISolitaryFeature = {
+      name: "feature",
+      minCardinality: "1",
+      maxCardinality: "1",
+      attributes: [a1],
+      parent: r,
+    };
+
+    let a2: fm.IAttribute = {
+      name: "attr2",
+      type: "dfsfdfsf",
+    };
+
+    let f2: fm.ISolitaryFeature = {
+      name: "feature drugi",
+      minCardinality: "1",
+      maxCardinality: "1",
+      attributes: [a2],
+      parent: r,
+    };
+
+    let diagram: fm.IFeatureDiagram = {
+      elements: [f1, r, f2],
+      root: r,
+    };
+
+    let elGrupe1: fm.IGroupElement = {
+      name: "elGrupe1",
+      attributes: [],
+      minCardinality: "2",
+      maxCardinality: "4",
+    };
+
+    let grupa: fm.IGroupFeature = {
+      name: "grupa",
+      attributes: [],
+      elements: [f1, elGrupe1], // ovde je namerno napravljena greska. Solitary feature (f1) ne bi trebalo da moze da bude element grupe
+      minCardinality: "0",
+      maxCardinality: "1",
+      parent: r,
+    };
+
+    let grupaPodgrupe: fm.ISolitaryFeature = {
+      name: "dete od elementa grupe",
+      attributes: [],
+      minCardinality: "1",
+      maxCardinality: "1",
+      parent: elGrupe1,
+    };
+
     console.log("proba testa");
     expect(true).toBeTruthy;
   });
--- libraries/aelastics-synthesis/src/feature-model/fm-meta.model.type.ts
+++ libraries/aelastics-synthesis/src/feature-model/fm-meta.model.type.ts
@@ -8,19 +8,18 @@ import { ModelElement, Model } from "generic-metamodel";
 
 export const FMModel_TypeSchema = t.schema("FMModelSchema");
 
-export const FMConcept = t.subtype(
-  ModelElement,
-  {},
-  "FMConcept",
-  FMModel_TypeSchema
-);
+// export const FMConcept = t.subtype(
+//   ModelElement,
+//   {},
+//   "FMConcept",
+//   FMModel_TypeSchema
+// );
 
-export const FeatureDiagram = t.subtype(
-  Model,
+export const FMConcept = t.object(
   {
-    root: t.link(FMModel_TypeSchema, "Root"),
+    name: t.string,
   },
-  "FeatureDiagram",
+  "FMConcept",
   FMModel_TypeSchema
 );
 
@@ -28,6 +27,7 @@ export const Attribute = t.subtype(
   FMConcept,
   {
     name: t.string,
+    type: t.string.derive("DataType").oneOf(["int", "string"]),
   },
   "Attribute",
   FMModel_TypeSchema
@@ -45,7 +45,31 @@ export const Feature = t.subtype(
 );
 
 export const Root = t.subtype(Feature, {}, "Root", FMModel_TypeSchema);
-export const Child = t.subtype(Feature, {}, "Child", FMModel_TypeSchema);
+
+// export const FeatureDiagram = t.subtype(
+//   Model,
+//   {
+//     root: Root,
+//   },
+//   "FeatureDiagram",
+//   FMModel_TypeSchema
+// );
+
+export const FeatureDiagram = t.object(
+  {
+    root: Root,
+    elements: t.arrayOf(FMConcept),
+  },
+  "FeatureDiagram",
+  FMModel_TypeSchema
+);
+
+export const Child = t.subtype(
+  Feature,
+  { parent: Feature },
+  "Child",
+  FMModel_TypeSchema
+);
 
 export const GroupElement = t.subtype(
   Feature,
@@ -74,8 +98,8 @@ export type IFMConcept = t.TypeOf<typeof FMConcept>;
 export type IAttribute = t.TypeOf<typeof Attribute>;
 export type IFeatureDiagram = t.TypeOf<typeof FeatureDiagram>;
 export type IFeature = t.TypeOf<typeof Feature>;
-export type Root = t.TypeOf<typeof Root>;
-export type Child = t.TypeOf<typeof Child>;
-export type GroupElement = t.TypeOf<typeof GroupElement>;
-export type SolitaryFeature = t.TypeOf<typeof SolitaryFeature>;
-export type GroupFeature = t.TypeOf<typeof GroupFeature>;
+export type IRoot = t.TypeOf<typeof Root>;
+export type IChild = t.TypeOf<typeof Child>;
+export type IGroupElement = t.TypeOf<typeof GroupElement>;
+export type ISolitaryFeature = t.TypeOf<typeof SolitaryFeature>;
+export type IGroupFeature = t.TypeOf<typeof GroupFeature>;
