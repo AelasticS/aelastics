8b5ccda8e614cbe19664058ec22afcac9c0150bd
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2022-11-23 00:19:37 +0100

    start ado metamodel

---
+++ libraries/aelastics-synthesis/src/feature-model/ado-meta.model.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) AelasticS 2022.
+ *
+ */
+
+import * as t from "aelastics-types";
+import { ModelElement, Model } from "generic-metamodel";
+
+export const FMConfigModel_TypeSchema = t.schema("FMConfigModel_TypeSchema");
