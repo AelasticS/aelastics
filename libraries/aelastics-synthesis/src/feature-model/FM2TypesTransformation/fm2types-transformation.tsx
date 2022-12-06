b20441e3a264e5b8f8e84b57d9bd28b3e12894bd
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2022-12-06 23:44:35 +0100

    dodaj FM2Types transformaciju

---
+++ libraries/aelastics-synthesis/src/feature-model/FM2TypesTransformation/fm2types-transformation.tsx
@@ -0,0 +1,120 @@
+/** @jsx hm */
+
+import { Element } from "../../jsx/element";
+import { hm } from "../../jsx/handle";
+import { ModelStore } from "../../jsx/ModelsStore";
+import { abstractM2M } from "../../transformations/abstractM2M";
+import * as fm from "../FM_MetaModel/fm-meta.model-V2.type";
+import { SpecPoint, SpecOption } from "../../transformations/spec-decorators";
+import {
+  Array,
+  Optional,
+  Property,
+  TypeModel,
+  Union,
+} from "../Types_MetaModel/types-components";
+import * as t from "../Types_MetaModel/types-meta.model";
+
+class FM2TypesTransformations extends abstractM2M<
+  fm.IFeatureDiagram,
+  t.ITypeModel
+> {
+  constructor(store: ModelStore) {
+    super(store);
+  }
+
+  template(fd: fm.IFeatureDiagram) {
+    return (
+      <TypeModel name={fd.name} MDA_level="M2" id={fd.id + 6}>
+        {fd.elements.map((e) => this.Feature2Type(e as fm.IFeature))}
+      </TypeModel>
+    );
+  }
+
+  Feature2Type(f: fm.IFeature): Element<t.IType> {
+    let type = undefined;
+    if (
+      f.maxCardinality == 1 ||
+      this.context.store.getTypeOf(f).isOfType(fm.GroupFeature)
+    ) {
+      type = this.Feature2Object(f);
+    } else {
+      type = this.Feature2Array(f);
+    }
+
+    if (f.minCardinality === 0) {
+      type = <Optional>{type}</Optional>;
+    }
+
+    return type;
+  }
+
+  @SpecPoint()
+  Feature2Object(f: fm.IFeature): Element<t.IType> {
+    return (
+      <Object name={f.name}>
+        {f.subfeatures?.map((e) => this.Feature2Type(e as fm.IFeature))}
+      </Object>
+    );
+  }
+
+  @SpecOption("Feature2Object", fm.SolitaryFeature)
+  Solitary2Object(f: fm.ISolitaryFeature): Element<t.IObject> {
+    // da li ovde treba staviti i mapiranje subfeature elemenata, posto se oni mapiraju i opstoj metodi
+
+    return (
+      <Object>
+        {f.attributes?.map((e) => this.Attribute2Property(e as fm.IAttribute))}
+        {f.subfeatures?.map((e) => this.Feature2Type(e as fm.IFeature))}
+      </Object>
+    );
+  }
+
+  @SpecOption("Feature2Object", fm.GroupFeature)
+  Group2Object(f: fm.IGroupFeature): Element<t.IObject> {
+    // da li ovde treba staviti i mapiranje subfeature elemenata, posto se oni mapiraju i opstoj metodi
+
+    return (
+      <Object>
+        {f.subfeatures?.map((e) => this.Feature2Type(e as fm.IFeature))}
+      </Object>
+    );
+  }
+
+  Attribute2Property(a: fm.IAttribute): Element<t.IProperty> {
+    return <Property name={a.name}></Property>;
+  }
+
+  Feature2Array(f: fm.IFeature): Element<t.IType> {
+    // ovde je dovoljno da se napravi jedan tip (taj konkretan)
+    return <Array>{this.Feature2Object(f)}</Array>;
+  }
+
+  //unija nije potrebna
+
+  //   Feature2Union(f: fm.IFeature): Element<t.IUnion> {
+  //     return (
+  //       <Union>
+  //         {f.subfeatures?.map((e) => this.Feature2Type(e as fm.IFeature))}
+  //       </Union>
+  //     );
+  //   }
+}
+
+// nije testirano. tek treba se testira
+
+describe("Test model transformations", () => {
+  it("tests eer to tables", () => {
+    let m = new FM2TypesTransformations(new ModelStore());
+    // let r = m.transform(s1)
+    // expect(r).toHaveProperty("name", "Persons")
+    // expect(r).toEqual(expect.objectContaining({
+    //     name: 'Persons',
+    //     elements: expect.arrayContaining([
+    //         expect.objectContaining({ name: "Person" }),
+    //         expect.objectContaining({ name: "PersonID" })
+    //     ])
+    // }
+    // ))
+  });
+});
