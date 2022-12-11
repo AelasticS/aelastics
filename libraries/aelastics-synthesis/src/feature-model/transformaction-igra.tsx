c735eae16a2584f39464d0d3fb4c3f4798d60cb6
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2022-12-11 11:15:08 +0100

    rename files

---
+++ libraries/aelastics-synthesis/src/feature-model/transformaction-igra.tsx
@@ -0,0 +1,25 @@
+// /** @jsx hm */
+
+// // import { Object, Property, hm, TypeModel } from "./components_trans";
+// import { IFeature, IFeatureDiagram } from "./FM_MetaModel/fm-meta.model-V2.type";
+// // import * as fmi from "./FM_MetaModel/fm_instance.test";
+// import { IObject } from "./Types_MetaModel/types-meta.model";
+
+// let f = (fmi: IFeatureDiagram) => {
+//   <TypeModel name={fmi.name}>
+//     {fmi.elements.map((e) => {
+//         feature2object(e);
+//     })}
+//     </TypeModel>;
+// };
+
+// let feature2object = (feature: IFeature) => {
+//   <Object name={feature.name}></Object>;
+// };
+
+// let o = (
+//   <Object name="dfsdf">
+//     <Property name="prop1"></Property>
+//     <Property name="prop2"></Property>
+//   </Object>
+// );
