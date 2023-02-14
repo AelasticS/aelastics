a10189c4ffc7a36d3b5095229c72acc79ec04aca
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2023-02-14 16:15:30 +0100

    add todo how to handle undefined in tsx

--- libraries/aelastics-synthesis/src/feature-model/Types_MetaModel/types.test.tsx
+++ libraries/aelastics-synthesis/src/feature-model/Types_MetaModel/types.test.tsx
@@ -49,25 +49,41 @@ const typeModel: Element<t.ITypeModel> = (
       <Property name="address" />
       <Property name="age" />
     </TypeObject>
+
+    <TypeObject name="Company">
+      <Property name="companyName">
+        <PropertyDomain $refByName="string" />
+      </Property>
+    </TypeObject>
+
+    <TypeOptional name="OptionalCompany">
+      <TypeOfOptional $refByName="Company" />
+    </TypeOptional>
+
     <TypeSubtype name="Worker">
       <Property name="professon" />
       <Property name="salary" />
       <TypeSupertype $refByName="Person"></TypeSupertype>
+      <Property name="workingCompany">
+        <PropertyDomain $refByName="OptionalCompany" />
+      </Property>
     </TypeSubtype>
-    <TypeObject name="Object">
-      <Property name="prop6" />
+    <TypeObject name="Place">
+      <Property name="name">
+        <PropertyDomain $refByName="string" />
+      </Property>
     </TypeObject>
 
     {/* prvi nacin. lose zato sto se pravi apstraktni tip, ako se zaboravi ref_local_id */}
-    <TypeOptional name="SomeOptionalType">
+    {/* <TypeOptional name="SomeOptionalType">
       <TypeOfOptional $refByName="Person"></TypeOfOptional>
       <TypeOfOptional $refByName="Person"></TypeOfOptional>
     </TypeOptional>
-    
+
     <TypeOptional name="SomeOptionalType2">
       <TypeOfOptional $refByName="Person"></TypeOfOptional>
       <TypeOfOptional $refByName="Person"></TypeOfOptional>
-    </TypeOptional>
+    </TypeOptional> */}
 
     {/* drugi nacin. testirati da li radi */}
 
@@ -129,26 +145,71 @@ describe("Type instance", () => {
         ]),
       })
     );
-
-    // TODO: srediti ako postoji bolji nacin
-    // provera da li je superType attributa na Subtype tipu
-    // expect((model.elements[5] as t.ISubtype).superType).toEqual(
-    //   model.elements[1] as t.IType
-    // );
   });
 
-  /*
   it("Test optional type", () => {
     expect(model).toEqual(
       expect.objectContaining({
-        name: "PrviTypeModel",
+        name: "FirstTypeModel",
         elements: expect.arrayContaining([
           expect.objectContaining({
-            name: "Opcioni tip",
+            name: "OptionalCompany",
           }),
         ]),
       })
     );
   });
-*/
+
+  it("Test optional property", () => {
+    expect(model).toEqual(
+      expect.objectContaining({
+        name: "FirstTypeModel",
+        elements: expect.arrayContaining([
+          expect.objectContaining({
+            name: "Worker",
+            properties: expect.arrayContaining([
+              expect.objectContaining({ name: "workingCompany" }),
+            ]),
+          }),
+        ]),
+      })
+    );
+  });
+
+  it("Test optional property relation", () => {
+    let workerType = model.elements.find((e) => e.name == "Worker");
+    let optCompanyProperty = (workerType as t.IObject).properties.find(
+      (e) => e.name == "workingCompany"
+    );
+
+    let optCompanyType = model.elements.find(
+      (e) => e.name == "OptionalCompany"
+    );
+
+    let companyType = model.elements.find(
+      (e) => e.name == "Company"
+    );
+
+    expect((optCompanyProperty as t.IProperty).domain).toBe(optCompanyType);
+    expect((optCompanyProperty as t.IProperty).domain).not.toBe(companyType);
+  });
+
+  // TODO: srediti ako postoji bolji nacin
+  // provera da li je superType attributa na Subtype tipu
+  // expect((model.elements[5] as t.ISubtype).superType).toEqual(
+  //   model.elements[1] as t.IType
+  // );
+
+  // it("Test optional type", () => {
+  //   expect(model).toEqual(
+  //     expect.objectContaining({
+  //       name: "PrviTypeModel",
+  //       elements: expect.arrayContaining([
+  //         expect.objectContaining({
+  //           name: "Opcioni tip",
+  //         }),
+  //       ]),
+  //     })
+  //   );
+  // });
 });
