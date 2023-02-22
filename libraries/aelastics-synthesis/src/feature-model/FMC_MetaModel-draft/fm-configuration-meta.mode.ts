6e614a6108352d1f5a9bdf357c34918b1648b5a2
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2023-02-22 13:40:04 +0100

    cleanup the FMConfig MM

---
+++ libraries/aelastics-synthesis/src/feature-model/FMC_MetaModel-draft/fm-configuration-meta.mode.ts
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) AelasticS 2022.
+ *
+ */
+
+import * as t from "aelastics-types";
+import {
+  FeatureDiagram as FM_FeatureDiagram,
+  Feature as FM_Feature,
+  Attribute as FM_Attribute,
+} from "../FM_MetaModel/fm-meta.model.type";
+import { ModelElement, Model } from "generic-metamodel";
+
+export const FMConfigModel_TypeSchema = t.schema("FMConfigModel_TypeSchema");
+
+export const Attribute = t.subtype(
+  ModelElement,
+  {
+    name: t.string,
+    value: t.string,
+    reference: FM_Attribute, // TODO It should refer to an Attribute or an IAttribute?
+  },
+  "Attribute",
+  FMConfigModel_TypeSchema
+);
+
+export const SelectedFeature = t.subtype(
+  ModelElement,
+  {
+    children: t.arrayOf(t.link(FMConfigModel_TypeSchema, "SelectedFeature")),
+    reference: FM_Feature, // TODO It should refer to a Feature or an IFeature?
+  },
+  "SelectedFeature",
+  FMConfigModel_TypeSchema
+);
+
+export const SolitaryFeature = t.subtype(
+  SelectedFeature,
+  {},
+  "SolitaryFeature",
+  FMConfigModel_TypeSchema
+);
+
+export const GroupFeature = t.subtype(
+  SelectedFeature,
+  {},
+  "GroupFeature",
+  FMConfigModel_TypeSchema
+);
+export const FMConfiguration = t.subtype(
+  Model,
+  {
+    selectedFeatures: t.arrayOf(SelectedFeature),
+    forModel: FM_FeatureDiagram, // TODO It should refer to a FeatureDiagram or an IFeatureDiagram?
+  },
+  "FMConfiguration",
+  FMConfigModel_TypeSchema
+);
+
+export type ISelectedFeature = t.TypeOf<typeof SelectedFeature>;
+export type IFMConfiguration = t.TypeOf<typeof FMConfiguration>;
+export type ISolitaryFeature = t.TypeOf<typeof SolitaryFeature>;
+export type IGroupFeature = t.TypeOf<typeof GroupFeature>;
+export type IAttribute = t.TypeOf<typeof Attribute>;
