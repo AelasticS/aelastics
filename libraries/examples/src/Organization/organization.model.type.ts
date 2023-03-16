509f06a2a86521e4dc4dcf523bdc59c74b91a9ba
Sinisa <sinisa.neskovic@users.noreply.github.com>
2023-03-16 02:24:25 +0100

    refactor Organization example

--- libraries/examples/src/Organization/organization.model.type.ts
+++ libraries/examples/src/Organization/organization.model.type.ts
@@ -6,13 +6,15 @@ export const ID = t.number.derive('IDType').positive
 export const Name = t.string.derive('NameType').alphanumeric.maxLength(128)
 export const Age = t.number.derive('Human age').int8.positive.inRange(1, 120);
 
+export const Child = t.object({name: t.string}, "Child")
+
 export const Employee = t.entity({
     id:ID,
     name: Name,
     age: t.optional(Age),
-   sex: t.string, // t.unionOf([t.literal('male'), t.literal("female")],"sexType"),
+    sex: t.string, // t.unionOf([t.literal('male'), t.literal("female")],"sexType"),
     birthPlace:  t.object({name: t.string, state: t.string}),
-    children: t.arrayOf(t.object({name: t.string}, "Child"))
+    children: t.arrayOf(Child)
 }, ["id"], 'Employee', OrgSchema);
 
 export const Organization = t.entity({
@@ -23,6 +25,7 @@ export const Organization = t.entity({
     departments: t.arrayOf(t.link(OrgSchema, "Organization")),
 }, ["id"], 'Organization', OrgSchema);
 
+export type IChild = t.TypeOf<typeof Child>
 export type IEmployee = t.TypeOf<typeof Employee>
 export type IOrganization = t.TypeOf<typeof Organization>
 
