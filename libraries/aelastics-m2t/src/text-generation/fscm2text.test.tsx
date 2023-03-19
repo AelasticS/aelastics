f9d94fcdd458e9d17453db569d0157874d3d68d6
Sinisa <sinisa.neskovic@users.noreply.github.com>
2023-03-20 00:48:52 +0100

    add testing conncetion of model elements

--- libraries/aelastics-m2t/src/text-generation/fscm2text.test.tsx
+++ libraries/aelastics-m2t/src/text-generation/fscm2text.test.tsx
@@ -40,34 +40,74 @@ let testModel2_Element: Element<IFS_Model> = (
     </FileModel>
 );
 
+
+let Doc1TopDir = <Doc name="Doc1">
+    <P>Title Doc1</P>
+    <Sec name="Chapter 1">
+        <Sec name="Subchapter 1.1">
+            <P>text of Subchapter 1.1</P>
+        </Sec>
+        <P>text of at end of Chapter 1</P>
+    </Sec>
+    <Sec name="Chapter 2">
+        <Sec name="Subchapter 2.1"></Sec>
+        <P>text of Subchapter 2.1</P>
+    </Sec>
+    <P>Conclusions</P>
+</Doc>
+
+let testModel3_Element: Element<IFS_Model> = (
+    <FileModel name="test model3" store={testStore}>
+        <Dir name="TopDir">
+            {Doc1TopDir}
+            <Dir name="Subdir1"></Dir>
+            <Dir name="Subdir2"></Dir>
+        </Dir>
+    </FileModel>
+);
+
 describe("test text generation", () => {
     it("should generate correct document content for testModel1", () => {
-        const testDoc1:IFS_Model = testModel1_Element.render(new Context());
+        const testDoc1: IFS_Model = testModel1_Element.render(new Context());
         expect(testDoc1.elements).toEqual(
             expect.arrayContaining([
-                expect.objectContaining({ name:"TestDoc1.txt"}),
-                expect.objectContaining({ txtContent:"some text"})
+                expect.objectContaining({ name: "TestDoc1.txt" }),
+                expect.objectContaining({ txtContent: "some text" })
             ])
-           
+
         )
     });
 
     it("should generate correct document content for testModel2", () => {
-        const testDoc2:IFS_Model = testModel2_Element.render(new Context());
+        const testDoc2: IFS_Model = testModel2_Element.render(new Context());
         expect(testDoc2.elements).toEqual(
             expect.arrayContaining([
-                expect.objectContaining({ name:"TestDoc2.txt"}),
-                expect.objectContaining({ txtContent:"some text for Math.log2(8)=3"})
+                expect.objectContaining({ name: "TestDoc2.txt" }),
+                expect.objectContaining({ txtContent: "some text for Math.log2(8)=3" })
             ])
-           
+
         )
         const doc = testDoc2.elements[0] as IDocument
         expect(doc.elements).toEqual(
             expect.arrayContaining([
-                expect.objectContaining({ txtContent:"some text for Math.log2(8)=3"})
+                expect.objectContaining({ txtContent: "some text for Math.log2(8)=3" })
             ])
-           
-        )  
+
+        )
+    });
+
+    it("should generate correct document content for testModel3", () => {
+        const testDoc3: IFS_Model = testModel3_Element.render(new Context());
+        expect(testDoc3.elements).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ name: "TopDir" }),
+                expect.objectContaining({ name: "Subdir1" }),
+                expect.objectContaining({ name: "Subdir2" }),
+                expect.objectContaining({ name: "Doc1" }),
+
+            ])
+        )
+ 
     });
 });
 
