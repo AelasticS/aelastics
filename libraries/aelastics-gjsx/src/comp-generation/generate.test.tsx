b5283c3ced094d6b8fff003bcb223624774ce22a
Sinisa <sinisa.neskovic@users.noreply.github.com>
2023-03-28 23:11:54 +0200

    add library aelastics-gjsx (generate JSX components)

---
+++ libraries/aelastics-gjsx/src/comp-generation/generate.test.tsx
@@ -0,0 +1,11 @@
+/** @jsx hm */
+import { hm } from "aelastics-synthesis";
+
+describe("test text generation", () => {
+    it("should generate correct document content for testModel1", async () => {
+        
+        expect(1).toEqual(1)
+        
+    });
+});
+
---
+++ libraries/aelastics-gjsx/src/comp-generation/generate.ts
@@ -0,0 +1,2 @@
+import { ModelStore } from "aelastics-synthesis";
+
