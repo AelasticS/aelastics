42fd5ec8b4f7a8288731e4c863da80663f714414
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2023-04-25 22:44:51 +0200

    M2T: fix generation of contained folders. fix duplicate elements when using refByName. add todos for text indents

--- libraries/aelastics-synthesis/src/m2t/text-generation/generate.test.tsx
+++ libraries/aelastics-synthesis/src/m2t/text-generation/generate.test.tsx
@@ -1,90 +1,107 @@
 /** @jsx hm */
-import { hm } from './../../index';
-import { generate, M2T, getResultByItemPath, M2T_Model, IParagraph, ISection } from "../index";
+import { hm } from "./../../index";
+import {
+  generate,
+  M2T,
+  getResultByItemPath,
+  M2T_Model,
+  IParagraph,
+  ISection,
+} from "../index";
 import { Dir, Doc, P, Sec } from "../index";
 import { IDirectory, IDocument } from "../index";
-import { ModelStore, Context, Element } from './../../index';
+import { ModelStore, Context, Element } from "./../../index";
 
 const testStore = new ModelStore();
 
 let testDoc1Element: Element<IDocument> = (
-    <Doc name="TestDoc1.txt">
-        <P>paragraph 1</P>
-        <P>paragraph 2</P>
-    </Doc>
+  <Doc name="TestDoc1.txt">
+    <P>paragraph 1</P>
+    <P>paragraph 2</P>
+  </Doc>
 );
 
 let testDoc2Element: Element<IDocument> = (
-    <Doc name="TestDoc2.txt">
-        <P>{`some text for Math.log2(8)=${Math.log2(8)}`}</P>
-    </Doc>
+  <Doc name="TestDoc2.txt">
+    <P>{`some text for Math.log2(8)=${Math.log2(8)}`}</P>
+  </Doc>
 );
 
 let dir1Element: Element<IDirectory> = (
-    <Dir name="directory1">
-        {testDoc1Element}
-        <Dir name="subDir1">
-            {testDoc2Element}
-        </Dir>
-    </Dir>
+  <Dir name="directory1">
+    {testDoc1Element}
+    <Dir name="subDir1">{testDoc2Element}</Dir>
+  </Dir>
 );
 
 let testModel1_Element: Element<M2T_Model> = (
-    <M2T name="test model1" store={testStore}>
-        {testDoc1Element}
-    </M2T>
+  <M2T name="test model1" store={testStore}>
+    {testDoc1Element}
+  </M2T>
 );
 
 let testModel2_Element: Element<M2T_Model> = (
-    <M2T name="test model2" store={testStore}>
-        {testDoc2Element}
-    </M2T>
+  <M2T name="test model2" store={testStore}>
+    {testDoc2Element}
+  </M2T>
 );
 
-
-let Doc1TopDir = <Doc name="Doc1">
+let Doc1TopDir = (
+  <Doc name="Doc.txt">
     <P>Title Doc1</P>
     <Sec name="Chapter 1">
-        <Sec name="Subchapter 1.1">
-            <P>text of Subchapter 1.1</P>
-        </Sec>
-        <P>text of at end of Chapter 1</P>
+      <Sec name="Subchapter 1.1">
+        <P>text of Subchapter 1.1</P>
+      </Sec>
+      <P>text of at end of Chapter 1</P>
     </Sec>
     <Sec name="Chapter 2">
-        <Sec name="Subchapter 2.1"></Sec>
+      <Sec name="Subchapter 2.1">
         <P>text of Subchapter 2.1</P>
+      </Sec>
     </Sec>
     <P>Conclusions</P>
-</Doc>
+    <Sec $refByName="Chapter 1">
+        <P>sdfdfsfdsfsdsfdsdsf</P>
+    </Sec>
+  </Doc>
+);
 
 let testModel3_Element: Element<M2T_Model> = (
-    <M2T name="test model3" store={testStore}>
-        <Dir name="TopDir">
-            {Doc1TopDir}
-            <Dir name="Subdir1"></Dir>
-            <Dir name="Subdir2"></Dir>
-        </Dir>
-    </M2T>
+  <M2T name="test model3" store={testStore}>
+    <Dir name="TopDir">
+      {Doc1TopDir}
+      <Dir name="Subdir1">
+        <Doc name="Doc1.txt">
+          <P>Title Doc1</P>
+          <P>Conclusions</P>
+        </Doc>
+      </Dir>
+      <Dir name="Subdir2"></Dir>
+    </Dir>
+  </M2T>
 );
 
 describe("test text generation", () => {
-    it("should generate correct document content for testModel1", async () => {
-        const testDoc1: M2T_Model = testModel1_Element.render(new Context());
-        const res = await generate(testStore, testDoc1, {rootDir:"TXT_Output", mode:"real"})
-        expect(res.noSuccesses).toEqual(1)
-        expect(res.noFailures).toEqual(0)
-        let s = res.results[0].outcome
-        //@ts-ignore
-        expect(s.value).toEqual("paragraph 1\nparagraph 2\n")
-        //@ts-ignore
-        expect(getResultByItemPath(res,"TestDoc1.txt")?.value).toEqual("paragraph 1\nparagraph 2\n")
-        
+  // it("should generate correct document content for testModel1", async () => {
+  //     const testDoc1: M2T_Model = testModel1_Element.render(new Context());
+  //     const res = await generate(testStore, testDoc1, {rootDir:"TXT_Output", mode:"real"})
+  //     expect(res.noSuccesses).toEqual(1)
+  //     expect(res.noFailures).toEqual(0)
+  //     let s = res.results[0].outcome
+  //     //@ts-ignore
+  //     expect(s.value).toEqual("paragraph 1\nparagraph 2\n")
+  //     //@ts-ignore
+  //     expect(getResultByItemPath(res,"TestDoc1.txt")?.value).toEqual("paragraph 1\nparagraph 2\n")
+
+  // });
+
+  it("should generate correct document content for testModel1", async () => {
+    const testDoc1: M2T_Model = testModel3_Element.render(new Context());
+    const res = await generate(testStore, testDoc1, {
+      rootDir: "TXT_Output2",
+      mode: "real",
     });
-
-    // it("should generate correct document content for testModel1", async () => {
-    //     const testDoc1: M2T_Model = testModel3_Element.render(new Context());
-    //     const res = await generate(testStore, testDoc1, {rootDir:"TXT_Output2", mode:"real"});
-    //     expect(true).toBeTruthy();
-    // });
+    expect(true).toBeTruthy();
+  });
 });
-
--- libraries/aelastics-synthesis/src/m2t/text-generation/generate.ts
+++ libraries/aelastics-synthesis/src/m2t/text-generation/generate.ts
@@ -12,146 +12,159 @@ import {
 import { GenContext } from "./gen-context";
 import { GenResult, ItemResult } from "./gen-result";
 import { failure, success } from "aelastics-result";
-import { createDir, createFile } from "./fs-operations"
-import { ModelStore } from './../../index';
-
+import { createDir, createFile } from "./fs-operations";
+import { ModelStore } from "./../../index";
 
 /**
  *  Options for generating text
  */
 export type Options = {
-  rootDir?: string,
-  mode?: "real" | "mock"
-}
-
+  rootDir?: string;
+  mode?: "real" | "mock";
+  indent?: boolean;
+};
 
 /**
  * Generate text files specified by a M2T model
- * @param store 
- * @param m  - model specifying text genaration 
+ * @param store
+ * @param m  - model specifying text genaration
  * @param options - generation options
- * @returns 
+ * @returns
  */
-export const generate = async (store: ModelStore, m: M2T_Model, options?: Options): Promise<GenResult> => {
-  const acting: boolean = false, rootDir: string = "output"
-  const ctx = new GenContext(store, options)
+export const generate = async (
+  store: ModelStore,
+  m: M2T_Model,
+  options?: Options
+): Promise<GenResult> => {
+  const acting: boolean = false,
+    rootDir: string = "output";
+  const ctx = new GenContext(store, options);
   try {
     if (ctx.realMode)
       // create top dir
-      createDir(ctx.rootDir)
+      createDir(ctx.rootDir);
   } catch (e) {
     const res: ItemResult = {
       itemID: "-1",
       itemPath: ctx.rootDir,
       itemType: "Dir",
-      outcome: failure(e as Error)
-    }
-    ctx.result.results.push(res)
+      outcome: failure(e as Error),
+    };
+    ctx.result.results.push(res);
     ctx.result.noFailures++;
-    return ctx.result
+    return ctx.result;
   }
 
   // generate model elements
-  m.elements.forEach(async (item) => {
-    if (store.isTypeOf(item, M2T_Item))
-      await generateItem(item, ctx.rootDir, ctx)
-  });
+  m.items.forEach(async (item) => await generateItem(item, ctx.rootDir, ctx));
+
   return ctx.result;
 };
 
-
 /**
- *  Generate directories and files specified by a file system model 
- * 
- * @param acting - if true, generate with an effect on real file system; otherwise do nothing 
- * @param rootDir - the start of directory hierarchy 
+ *  Generate directories and files specified by a file system model
+ *
+ * @param acting - if true, generate with an effect on real file system; otherwise do nothing
+ * @param rootDir - the start of directory hierarchy
  * @returns - results of execution; returned even if acting param is false
  */
 
 /**
- * 
- * @param i 
- * @param path 
- * @param ctx 
- * @returns 
+ *
+ * @param i
+ * @param path
+ * @param ctx
+ * @returns
  */
 const generateItem = async (i: IM2T_Item, path: string, ctx: GenContext) =>
   isDirectory(i)
     ? generateDirectory(i, path, ctx)
-    : generateDocument(<IDocument>i, path, ctx)
+    : generateDocument(<IDocument>i, path, ctx);
 
-const generateDirectory = async (dir: IDirectory, path: string, ctx: GenContext) => {
+const generateDirectory = async (
+  dir: IDirectory,
+  path: string,
+  ctx: GenContext
+) => {
+
+  const currPath = `${path}/${dir.name}`;
+  
   try {
-    if (ctx.realMode)
-      await createDir(dir.name)
+    if (ctx.realMode) await createDir(currPath);
     const res: ItemResult = {
       itemID: dir.id,
       itemPath: dir.name,
       itemType: "Dir",
-      outcome: success("successfully created")
-    }
-    ctx.result.results.push(res)
+      outcome: success("successfully created"),
+    };
+    ctx.result.results.push(res);
     ctx.result.noSuccesses++;
-  }
-  catch (e) {
+  } catch (e) {
     const res: ItemResult = {
       itemID: dir.id,
       itemPath: dir.name,
       itemType: "Dir",
-      outcome: failure(e as Error)
-    }
-    ctx.result.results.push(res)
+      outcome: failure(e as Error),
+    };
+    ctx.result.results.push(res);
     ctx.result.noFailures++;
-    return // stop generation further for this dir
+    return; // stop generation further for this dir
   }
-  const currPath = `${path}/${dir.name}`
+  
   // execute files in the directory
   dir.items.forEach(async (item) => await generateItem(item, currPath, ctx));
-
 };
 
 /**
- * 
- * @param doc 
- * @param path 
- * @param ctx 
+ *
+ * @param doc
+ * @param path
+ * @param ctx
  */
-const generateDocument = async (doc: IDocument, path: string, ctx: GenContext) => {
+const generateDocument = async (
+  doc: IDocument,
+  path: string,
+  ctx: GenContext
+) => {
   try {
     let buffer: Array<string> = [];
     doc.elements.forEach((s) => buffer.push(generateFileElement(s)));
     const text = buffer.join("");
-    const currPath = `${path}/${doc.name}`
-    if (ctx.realMode)
-      await createFile(currPath, text)
+    const currPath = `${path}/${doc.name}`;
+    if (ctx.realMode) await createFile(currPath, text);
 
     const res: ItemResult = {
       itemID: doc.id,
       itemPath: doc.name,
       itemType: "Doc",
-      outcome: success(text)
-    }
-    ctx.result.results.push(res)
+      outcome: success(text),
+    };
+    ctx.result.results.push(res);
     ctx.result.noSuccesses++;
   } catch (e) {
     const res: ItemResult = {
       itemID: doc.id,
       itemPath: doc.name,
       itemType: "Doc",
-      outcome: failure(e as Error)
-    }
-    ctx.result.results.push(res)
+      outcome: failure(e as Error),
+    };
+    ctx.result.results.push(res);
     ctx.result.noFailures++;
   }
 };
 
 /**
- * 
- * @param input 
- * @returns 
+ *
+ * @param input
+ * @returns
  */
+
+//TODO: add print context and indentation level to arguments to generateFileElement
+//TODO: fix indent when paragraph is multiline
 const generateFileElement = (input: IDocElement): string =>
-  isParagraph(input) ? `${input.txtContent}\n` : generateSection(<ISection>input);
+  isParagraph(input)
+    ? `${input.txtContent}\n`
+    : generateSection(<ISection>input);
 
 const generateSection = (input: ISection): string => {
   let buffer: Array<string> = [];
@@ -160,17 +173,17 @@ const generateSection = (input: ISection): string => {
 };
 
 /**
- * Get a result about a specific directory or file 
- * @param res - result about execeuting 
- * @param id 
- * @returns 
+ * Get a result about a specific directory or file
+ * @param res - result about execeuting
+ * @param id
+ * @returns
  */
 export const getResultByItemID = (res: GenResult, id: string) => {
-  const r = res.results.find(item => item.itemID === id)
-  return r ? r.outcome : undefined
-}
+  const r = res.results.find((item) => item.itemID === id);
+  return r ? r.outcome : undefined;
+};
 
 export const getResultByItemPath = (res: GenResult, path: string) => {
-  const r = res.results.find(item => item.itemPath === path)
-  return r ? r.outcome : undefined
-}
+  const r = res.results.find((item) => item.itemPath === path);
+  return r ? r.outcome : undefined;
+};
