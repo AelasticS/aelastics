52dd6ec4c4fd9cc871ba3fe94efa788aa7226e25
Dejan Stojimirovic <stojimirovic.dejan@users.noreply.github.com>
2023-04-25 22:54:02 +0200

    Types2TextModel: add test file

---
+++ libraries/aelastics-synthesis/src/types-metamodel/generate-code/types2text.model.test.tsx
@@ -0,0 +1,23 @@
+/** @jsx hm */
+
+import { Element, ElementInstance } from "../../jsx/element";
+import { hm } from "../../jsx/handle";
+import * as t from "../types-meta.model";
+import {
+  TypeObject,
+  Property,
+  TypeSupertype,
+  TypeModel,
+  TypeSubtype,
+  TypeOptional,
+  TypeOfOptional,
+  PropertyDomain,
+} from "./../types-components";
+import { Context } from "../../jsx/context";
+import { ModelStore } from "../../index";
+
+describe("test types2text transormations", () => {
+  it("should create correct document", async () => {
+    expect(true).toBeTruthy();
+  });
+});
