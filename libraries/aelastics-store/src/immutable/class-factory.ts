521ca3ddc9676e2d70dd26751ea2ec1367077975
Sinisa <sinisa.neskovic@users.noreply.github.com>
2023-07-13 01:48:00 +0200

    refactor class-factory

--- libraries/aelastics-store/src/immutable/class-factory.ts
+++ libraries/aelastics-store/src/immutable/class-factory.ts
@@ -1,50 +1,46 @@
+import { ServiceError } from "aelastics-result";
 import { Any, AnyObjectType, ObjectLiteral } from "aelastics-types";
 import { immerable } from "immer";
 
-type Class<P> = { new(): P };
+type Class<P> = { new (): P };
 
-class ClassFactory<P extends ObjectLiteral> {
-  private _protoObject:ObjectLiteral = {}
-  private _props:Map<string, Any>
-  private _class:Class<P>
-
-  constructor(readonly type:AnyObjectType) {
-    this._props = type.allProperties
-    this.createPrototype()
-    this._class = this.createClass()
+export function createClass<P extends ObjectLiteral>(type: AnyObjectType, idMap: Map<any, ObjectLiteral>): Class<P> {
+  const props = type.allProperties;
+  const allInverse = type.allInverse;
+  function ConstrFun(this: ObjectLiteral, init: Partial<P>) {
+    // init props
+    for (const [p, pType] of props) {
+      this.p = init?.p;
+    }
+    // make it compatible with immer
+    Object.assign(this, { [immerable]: true });
   }
+  // set prototype
+  createPrototype(ConstrFun.prototype, props,idMap)
+  return ConstrFun as any as Class<P>;
+}
 
-  private createPrototype() {
-    // create getters and setters in the prototype
-    for (const [p, pType] of this._props) {
-      if (pType.typeCategory === "Object") {
-        Object.defineProperty(this._protoObject, p, {
+function createPrototype(protoObject:{}, props: Map<string, Any>, idMap: Map<any, ObjectLiteral>) {
+  // create getters and setters in the prototype
+  for (const [p, pType] of props) {
+    if (pType.typeCategory === "Object") {
+      const objType: AnyObjectType = pType as AnyObjectType;
+      if (objType.isEntity) {
+        Object.defineProperty(protoObject, p, {
           get: function (this: ObjectLiteral) {
-            return this[`_${p}`];
+            return idMap.get(this[`_${p}`]);
           },
-          set: function (this: ObjectLiteral, value: any) {
-            if (this[`_${p}`] !== value) this[`_${p}`] = value;
+          set: function (this: ObjectLiteral, value: ObjectLiteral) {
+            if (objType.identifier.length > 1)
+              new ServiceError(
+                "FeatureNotSupported",
+                `Entity ${objType.name} has complex identifier`
+              );
+            let id = value[objType.identifier[0]];
+            if (this[`_${p}`] !== value[id]) this[`_${p}`] = value[id];
           },
         });
       }
     }
   }
-  
-  createClass(): Class<P> {
-    const props = this.type.allProperties;
-    const allInverse = this.type.allInverse;
-    function ConstrFun(this: ObjectLiteral) {
-      // make it compatible with immer
-      Object.assign(this, { [immerable]: true });
-  
-      // init props
-      for (const [p, pType] of props) {
-       // this[`_${p}`] = init?.p;
-      }
-    }
-  
-    return ConstrFun as any as Class<P>;
-  }
-  
 }
-
