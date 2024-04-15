31e7ee586924d29e03b50654b775be22ec284c8d
ebrvc <89137171+ebrvc@users.noreply.github.com>
2024-04-15 23:24:26 +0200

    fix: fix all getters and setters in the example

--- libraries/examples/src/university-complex-state/university.model.type.ts
+++ libraries/examples/src/university-complex-state/university.model.type.ts
@@ -3,49 +3,73 @@ import * as t from "aelastics-types"
 export const UniversitySchema = t.schema("UniversitySchema")
 
 export const ID = t.string
-export const Name = t.string.derive('Valid name').alphanumeric.maxLength(128)
+export const Name = t.string.derive("Valid name").alphanumeric.maxLength(128)
 export const Email = t.string.derive("Valid email").email
-export const Age = t.number.derive('Valid student age').int8.positive.inRange(18, 99);
-export const Grade = t.number.derive("Valid grade scale").int8.positive.inRange(1, 12);
+export const Age = t.number.derive("Valid student age").int8.positive.inRange(18, 99)
+export const Grade = t.number.derive("Valid grade scale").int8.positive.inRange(1, 12)
 
-
-export const Program = t.entity({
+export const Program = t.entity(
+  {
     id: ID,
     name: Name,
     courses: t.arrayOf(t.link(UniversitySchema, "Course", "ProgramToCourseLink")),
-    enrolledStudents:  t.arrayOf(t.link(UniversitySchema, "Student", "ProgramToStudentLink")),
-}, ["id"], "Program", UniversitySchema)
+    enrolledStudents: t.arrayOf(t.link(UniversitySchema, "Student", "ProgramToStudentLink")),
+  },
+  ["id"],
+  "Program",
+  UniversitySchema
+)
 
-export const Course = t.entity({
+export const Course = t.entity(
+  {
     id: ID,
     name: Name,
     program: Program,
-    students: t.arrayOf(t.link(UniversitySchema, 'Student', 'CourseToStudentLink')),
-    assignment: t.arrayOf(t.link(UniversitySchema, 'Assignment', 'CourseToAssignmentLink'))
-}, ["id"], "Course", UniversitySchema)
+    students: t.arrayOf(t.link(UniversitySchema, "Student", "CourseToStudentLink")),
+    assignments: t.arrayOf(t.link(UniversitySchema, "Assignment", "CourseToAssignmentLink")),
+  },
+  ["id"],
+  "Course",
+  UniversitySchema
+)
 
-export const Assignment = t.entity({
+export const Assignment = t.entity(
+  {
     id: ID,
     name: Name,
-    description: t.string.alphanumeric.maxLength(250)
- }, ["id"], "Assignment", UniversitySchema)
+    description: t.string.alphanumeric.maxLength(250),
+    course: t.link(UniversitySchema, "Course", "AssignmentToCourseLink"),
+  },
+  ["id"],
+  "Assignment",
+  UniversitySchema
+)
 
-export const Submission = t.entity({
+export const Submission = t.entity(
+  {
     id: ID,
     student: t.link(UniversitySchema, "Student", "SubmissionToStudentLink"),
     assignment: t.link(UniversitySchema, "Assignment", "SubmissionToAssignmentLink"),
     content: t.string,
-    grade: Grade
-}, ["id"], "Submission", UniversitySchema)
+    grade: Grade,
+  },
+  ["id"],
+  "Submission",
+  UniversitySchema
+)
 
-export const Student = t.entity({
+export const Student = t.entity(
+  {
     id: ID,
     name: Name,
     email: Email,
     enrolledProgram: Program,
-    approvedCourses: t.arrayOf(t.link(UniversitySchema, 'Course', 'StudentToCourseLink')),
-}, ["id"], "Student", UniversitySchema)
-
+    approvedCourses: t.arrayOf(t.link(UniversitySchema, "Course", "StudentToCourseLink")),
+  },
+  ["id"],
+  "Student",
+  UniversitySchema
+)
 
 export type IProgram = t.TypeOf<typeof Program>
 export type ICourse = t.TypeOf<typeof Course>
--- libraries/examples/src/university-complex-state/university.ts
+++ libraries/examples/src/university-complex-state/university.ts
@@ -1,122 +1,182 @@
-import { IAssignment, ICourse, IProgram, IStudent, ISubmission } from "./university.model.type";
+import { IAssignment, ICourse, IProgram, IStudent, ISubmission } from "./university.model.type"
 
-// one store? multiple stores?
 const store = {
-    programs: new Map<string, IProgram>(),
-    courses: new Map<string, ICourse>(),
-    students: new Map<string, IStudent>(),
-    submissions: new Map<string, ISubmission>(),
-    assignments: new Map<string, IAssignment>()
-};
-
-// TODO: add _ before every private variable
+  programs: new Map<string, IProgram>(),
+  courses: new Map<string, ICourse>(),
+  students: new Map<string, IStudent>(),
+  submissions: new Map<string, ISubmission>(),
+  assignments: new Map<string, IAssignment>(),
+}
 
 export class Program implements IProgram {
-    id: string;
-    name: string;
-    courses: string[];
-    enrolledStudents: Student[];
+  id: string
+  name: string
 
-    constructor(id: string, name: string) {
-        this.id = id;
-        this.name = name;
-        this.courses = [];
-        this.enrolledStudents = [];
-        store.programs.set(id, this);
-    }
+  private _courses: string[]
+  private _enrolledStudents: string[]
 
-    set course(c: ICourse) {
-        if (!this.courses.find(courseId => {courseId === c.id})) {
-            this.courses.push(c.id)
-        }
-    }
+  constructor(id: string, name: string) {
+    this.id = id
+    this.name = name
+    this._courses = []
+    this._enrolledStudents = []
+    store.programs.set(id, this)
+  }
 
-    getCourses(): ICourse[] {
-        return this.courses.map(courseId => store.courses.get(courseId)!);
-    }
+  set courses(c: ICourse[]) {
+    c.forEach((course) => {
+      if (!this._courses.find((courseId) => courseId === course.id)) {
+        this._courses.push(course.id)
+      }
+    })
+  }
 
-    setEnrolledStudents(s: string) {
-        // TODO: set student in parameter.
-    }
+  get courses(): ICourse[] {
+    return this._courses.map((courseId) => store.courses.get(courseId)!)
+  }
 
-    getEnrolledStudents() {
-        // TODO: return all students whose Ids are present in this.enrolledStudents.
-    }
+  set enrolledStudents(s: IStudent[]) {
+    s.forEach((student) => {
+      if (!this._enrolledStudents.find((studentId) => studentId === student.id)) {
+        this._enrolledStudents.push(student.id)
+      }
+    })
+  }
+
+  get enrolledStudents(): IStudent[] {
+    return this._enrolledStudents.map((studentId) => store.students.get(studentId)!)
+  }
 }
 
 export class Course implements ICourse {
-    id: string;
-    name: string;
-    _program: string;
-    students: Student[];
-    assignment: Assignment[];
+  id: string
+  name: string
 
-    constructor(id: string, name: string, program: string) {
-        this.id = id;
-        this.name = name;
-        this._program = program;
-        this.students = [];
-        this.assignment = [];
-        store.courses.set(id, this);
-    }
+  private _program: string
+  private _students: string[]
+  private _assignments: string[]
 
-    set program(p: IProgram) {
-        this._program = p.id
-    }
+  constructor(id: string, name: string, program: Program) {
+    this.id = id
+    this.name = name
+    this._program = program.id
+    this._students = []
+    this._assignments = []
+    store.courses.set(id, this)
+  }
 
-    get program() {
-        return store.programs.get(this._program)!
+  set program(p: IProgram) {
+    this._program = p.id
+  }
+
+  get program() {
+    return store.programs.get(this._program)!
+  }
+
+  set students(s: IStudent) {
+    if (!this._students.find((studentId) => studentId === s.id)) {
+      this._students.push(s.id)
     }
+  }
+
+  get students(): IStudent[] {
+    return this._students.map((studentId) => store.students.get(studentId)!)
+  }
+
+  set assignments(a: IAssignment) {
+    if (!this._assignments.find((assignmentId) => assignmentId === a.id)) {
+      this._assignments.push(a.id)
+    }
+  }
+
+  get assignments(): IAssignment[] {
+    return this._assignments.map((assignmentId) => store.assignments.get(assignmentId)!)
+  }
 }
 
 export class Submission implements ISubmission {
-    id: string;
-    student: Student;
-    assignment: Assignment;
-    content: string;
-    grade: number;
+  id: string
+  student: Student
+  content: string
+  grade: number
 
-    constructor(id: string, student: Student, assignment: Assignment, content: string, grade: number) {
-        this.id = id;
-        this.student = student;
-        this.assignment = assignment;
-        this.content = content;
-        this.grade = grade;
-        store.submissions.set(id, this);
+  private _assignment: string
 
-    }
+  constructor(id: string, student: Student, assignment: Assignment, content: string, grade: number) {
+    this.id = id
+    this.student = student
+    this._assignment = assignment.id
+    this.content = content
+    this.grade = grade
+    store.submissions.set(id, this)
+  }
+
+  set assignment(a: IAssignment) {
+    this._assignment = a.id
+  }
+
+  get assignment() {
+    return store.assignments.get(this._assignment)!
+  }
 }
 
 export class Student implements IStudent {
-    id: string;
-    name: string;
-    email: string;
-    enrolledProgram: Program;
-    approvedCourses: Course[];
+  id: string
+  name: string
+  email: string
 
-    constructor(id: string, name: string, email: string, enrolledProgram: Program, approvedCourses: Course[]) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.enrolledProgram = enrolledProgram;
-        this.approvedCourses = approvedCourses;
-        store.students.set(id, this);
-    }
+  private _enrolledProgram: string
+  private _approvedCourses: string[]
+
+  constructor(id: string, name: string, email: string, program: Program) {
+    this.id = id
+    this.name = name
+    this.email = email
+    this._enrolledProgram = program.id
+    this._approvedCourses = []
+    store.students.set(id, this)
+  }
+
+  set enrolledProgram(p: IProgram) {
+    this._enrolledProgram = p.id
+  }
+  get enrolledProgram() {
+    return store.programs.get(this._enrolledProgram)!
+  }
+
+  set approvedCourses(c: ICourse[]) {
+    c.forEach((course) => {
+      if (!this._approvedCourses.find((courseId) => courseId === course.id)) {
+        this._approvedCourses.push(course.id)
+      }
+    })
+  }
+
+  get approvedCourses(): ICourse[] {
+    return this._approvedCourses.map((courseId) => store.courses.get(courseId)!)
+  }
 }
 
 export class Assignment implements IAssignment {
-    id: string;
-    name: string;
-    description: string;
+  id: string
+  name: string
+  description: string
 
-    constructor(id: string, name: string, description: string) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        store.assignments.set(id, this);
-    }
+  private _course: string
+
+  constructor(id: string, name: string, description: string, course: Course) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this._course = course.id
+    store.assignments.set(id, this)
+  }
+
+  set course(c: ICourse) {
+    this._course = c.id
+  }
+
+  get course() {
+    return store.courses.get(this._course)!
+  }
 }
-
-
-
-
