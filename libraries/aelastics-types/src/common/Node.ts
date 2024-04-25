fab14e01b70e0ea3a3565d00f91ec119e447f0df
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2024-04-26 00:06:24 +0200

    docuemnt node.ts

--- libraries/aelastics-types/src/common/Node.ts
+++ libraries/aelastics-types/src/common/Node.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) AelasticS 2020.
+ * Copyright (c) A. AelasticS 2020.
  */
 
 import { Any } from "./DefinitionAPI";
@@ -8,6 +8,9 @@ import { VisitedNodes } from "./VisitedMap";
 import { AnnotationProcessor } from "../transducers/AnnotationProcessor";
 import { TypedAnnotation } from "../annotations/Annotation";
 
+/**
+ * Represents a node in a data structure that is aware of its type and annotations.
+ */
 export class Node {
   instance: any;
   type: Any;
@@ -19,6 +22,16 @@ export class Node {
   annotationProcessor: Map<TypedAnnotation, AnnotationProcessor>;
   private _revisited: boolean = false;
 
+  /**
+   * Constructs a new Node.
+   *
+   * @param i The instance data of the node.
+   * @param t The type descriptor of the node.
+   * @param acc Accumulator for carrying additional data.
+   * @param e Extra info associated with the node.
+   * @param parent The parent node in the hierarchy, if any.
+   * @param typeLevel Indicates if the node processing is at type level.
+   */
   constructor(
     i: any,
     t: Any,
@@ -31,28 +44,58 @@ export class Node {
     this.type = t;
     this.acc = acc;
     this.extra = e;
-    this.annotationProcessor = parent ? parent.annotationProcessor : new Map();
     this.parent = parent;
     this.visited = this.parent !== undefined ? this.parent.visited : new VisitedNodes();
     this.typeLevel = this.parent !== undefined ? this.parent.typeLevel : typeLevel;
+    this.annotationProcessor = parent ? parent.annotationProcessor : new Map();
   }
 
+  /**
+   * Returns whether the node has been revisited.
+   */
   get isRevisited(): boolean {
     return this._revisited;
   }
 
+  /**
+   * Sets the revisited status of the node.
+   */
   set isRevisited(b: boolean) {
     this._revisited = b;
   }
 
+  /**
+   * Retrieves the current annotation element for a given annotation type.
+   *
+   * @param a The annotation type.
+   * @returns The current annotation element, if any.
+   */
   getCurrentAnnotationElement(a: TypedAnnotation): any {
     return this.annotationProcessor.get(a)?.currentAnnotationElement;
   }
 
+  /**
+   * Retrieves the AnnotationProcessor for a given annotation type.
+   *
+   * @param a The annotation type.
+   * @returns The corresponding AnnotationProcessor, if present.
+   */
   getAnnotationProcessor(a: TypedAnnotation): AnnotationProcessor | undefined {
     return this.annotationProcessor.get(a);
   }
 
+  /**
+   * Factory method to create a Node. Reuses existing Node if input is already a Node,
+   * otherwise creates a new Node based on provided parameters.
+   *
+   * @param input The input data or existing Node.
+   * @param type The type descriptor.
+   * @param acc Accumulator for carrying additional data.
+   * @param e Extra info, defaults to undefined.
+   * @param parent The parent Node, if any.
+   * @param typeLevel Indicates if the node processing is at type level.
+   * @returns A new or existing Node instance.
+   */
   static makeNode(
     input: any | Node,
     type: Any,
@@ -62,13 +105,10 @@ export class Node {
     typeLevel: boolean = false
   ): Node {
     if (input instanceof Node) return input;
-    // check for subtypes
-    let implTypeName = type.name;
-    if (input) {
-      let implTypeName = input["@@aelastics/type"];
-      if (implTypeName !== "") {
-        const subType = type.ownerSchema.getType(implTypeName);
-        if (subType) return new Node(input, subType, acc, e, parent, typeLevel);
+      if (input && input["@@aelastics/type"] !== "") {
+      const subType = type.ownerSchema.getType(input["@@aelastics/type"]);
+      if (subType) {
+        return new Node(input, subType, acc, e, parent, typeLevel);
       }
     }
     return new Node(input, type, acc, e, parent, typeLevel);
