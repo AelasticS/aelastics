8107d0253350aba629bd600c8fcc5d3d4d3ce645
Martin Lupa <80256770+MartinLupa@users.noreply.github.com>
2024-05-10 12:57:05 +0200

    chore: add docs

--- libraries/aelastics-store/src/immutable/immutable-store.ts
+++ libraries/aelastics-store/src/immutable/immutable-store.ts
@@ -1,4 +1,4 @@
-import { AnyObjectType, ObjectLiteral, object } from "aelastics-types"
+import { AnyObjectType, ObjectLiteral } from "aelastics-types"
 import { Class, createClass } from "./createClass"
 import { OperationContext } from "./operation-context"
 import { immerable, produce, enableMapSet } from "immer"
@@ -16,6 +16,10 @@ enableMapSet()
 
 // TODO: implement immer inside the syncIdMapWithState function if needed.
 
+/**
+ * Implements an immutable state management store using Immer library to handle state updates immutably.
+ * The store supports objects that are identifiable by unique IDs ("@@aelastics/ID").
+ */
 class ImmerState {
   [immerable] = true
   constructor(readonly state: any) {}
@@ -30,10 +34,20 @@ export class ImmutableStore<S extends { [key: string]: IdentifiableItem[] }> {
   private _state: S
   ctx = new OperationContext()
 
+  /**
+   * Creates an instance of ImmutableStore.
+   * @param {S} initialState - The initial state of the store.
+   */
   constructor(initialState: S) {
     this._state = initialState
   }
 
+  /**
+   * Creates a new object of the specified type and initializes it with provided properties.
+   * @param {AnyObjectType} objectType - The type of object to create.
+   * @param {Partial<ObjectLiteral>} initProps - Initial properties to set on the new object.
+   * @returns {ObjectLiteral} The newly created object.
+   */
   newObject(objectType: AnyObjectType, initProps: Partial<ObjectLiteral> = {}): ObjectLiteral {
     let c = this._classMap.get(objectType)
 
@@ -45,6 +59,12 @@ export class ImmutableStore<S extends { [key: string]: IdentifiableItem[] }> {
     return this.ctx.createObject(c, initProps, objectType)
   }
 
+  /**
+   * Adds an object to the store under the specified key.
+   * @param {keyof S} key - The key under which the object should be added.
+   * @param {any} object - The object to add to the store.
+   * @throws {Error} if the key is not an array or does not exist on the state.
+   */
   addObject(key: keyof S, object: any): void {
     if (Array.isArray(this._state[key])) {
       ;(this._state[key] as Array<any>).push(object)
@@ -53,16 +73,10 @@ export class ImmutableStore<S extends { [key: string]: IdentifiableItem[] }> {
     }
   }
 
-  // produce(f: (draft: S, store: ImmutableStore<S>) => void) {
-  //   const { state, map } = produce(new ImmerState(this._state, this.ctx.idMap), (imm: ImmerState) => f(imm.state, this))
-  //   this._state = state
-  //   this.ctx.idMap = map
-  // }
-
-  // produce(f: (draft: any) => void): void {
-  //   this._state = produce(this._state, f)
-  // }
-
+  /**
+   * Applies a function to modify the store's state immutably.
+   * @param {(draft: any) => void} f - A function that receives the current state as a draft and modifies it.
+   */
   produce(f: (draft: any) => void) {
     const { state } = produce(new ImmerState(this._state), (imm: ImmerState) => {
       f(imm.state)
@@ -71,6 +85,12 @@ export class ImmutableStore<S extends { [key: string]: IdentifiableItem[] }> {
     this._state = state
   }
 
+  /**
+   * Synchronizes the identity map with the current state.
+   * @param {any} state - The current state of the store.
+   * @param {any} map - The existing map of IDs to objects.
+   * @returns {Map<string, any>} The updated map.
+   */
   syncIdMapWithState(state: any, map: any): Map<string, any> {
     for (const key of Object.keys(state)) {
       if (Array.isArray(state[key])) {
@@ -84,6 +104,10 @@ export class ImmutableStore<S extends { [key: string]: IdentifiableItem[] }> {
     return map
   }
 
+  /**
+   * Retrieves the current state of the store.
+   * @returns {S} The current state.
+   */
   getState(): S {
     return this._state
   }
