3a87ede101a85fd531e021bbfc5f3f2c6fcb91a4
ebrvc <89137171+ebrvc@users.noreply.github.com>
2024-05-21 15:50:58 +0200

    reproductible simple implementation our our issues

---
+++ libraries/aelastics-store/src/test-implementation/test.test.ts
@@ -0,0 +1,108 @@
+import { iFoo, ImmutableTestStore, TestStore } from "./test"
+
+let parent: iFoo
+let child: iFoo
+let store: TestStore
+let immutableStore: ImmutableTestStore
+
+describe("produce only the state", () => {
+  beforeAll(() => {
+    store = new TestStore()
+    parent = store.createObj("1", "parent")
+    child = store.createObj("2", "child")
+  })
+  test("Add stuff to state", () => {
+    store.produce((draft) => {
+      draft.push(parent)
+      draft.push(child)
+    })
+    const state = store.getState()
+
+    expect(state[0]).toBe(parent)
+    expect(state[1]).toBe(child)
+  })
+
+  test("Add relation between foos", () => {
+    store.produce((draft) => {
+      draft[1].parent = parent
+    })
+    const state = store.getState()
+
+    expect(state[0]?.parent).toBe(parent)
+    expect(state[1]?.child).toBe(child)
+  })
+
+  test("change name of the nested parent", () => {
+    store.produce((draft) => {
+      draft[1].parent.name = "new name"
+    })
+  })
+})
+// ------------------------------------------------------------
+// describe("produce the state and the map", () => {
+//   beforeAll(() => {
+//     store = new TestStore()
+//     parent = store.createObj("1", "parent")
+//     child = store.createObj("2", "child")
+//   })
+//   test("Add stuff to state", () => {
+//     store.produceWithIdMap((draft) => {
+//       draft.push(parent)
+//       draft.push(child)
+//     })
+//     const state = store.getState()
+
+//     expect(state[0]).toBe(parent)
+//     expect(state[1]).toBe(child)
+//   })
+
+//   test("Add relation between foos", () => {
+//     store.produceWithIdMap((draft) => {
+//       draft[1].parent = parent
+//     })
+//     const state = store.getState()
+
+//     expect(state[0]?.parent).toBe(parent)
+//     expect(state[1]?.child).toBe(child)
+//   })
+
+//   test("change name of the nested parent", () => {
+//     store.produceWithIdMap((draft) => {
+//       draft[1].parent.name = "new name"
+//     })
+//   })
+// })
+// ------------------------------------------------------------
+// describe("produce the entire store", () => {
+//   beforeAll(() => {
+//     immutableStore = new ImmutableTestStore()
+//     parent = immutableStore.createObj("1", "parent")
+//     child = immutableStore.createObj("2", "child")
+//   })
+//   test("Add stuff to state", () => {
+//     immutableStore = immutableStore.produce((draft) => {
+//       draft.push(parent)
+//       draft.push(child)
+//     })
+//     const state = immutableStore.getState()
+
+//     expect(state[0]).toBe(parent)
+//     expect(state[1]).toBe(child)
+//   })
+
+//   test("Add relation between foos", () => {
+//     immutableStore = immutableStore.produce((draft) => {
+//       draft[1].parent = parent
+//     })
+//     const state = immutableStore.getState()
+
+//     expect(state[0]?.parent).toBe(parent)
+//     expect(state[1]?.child).toBe(child)
+//   })
+
+//   test("change name of the nested parent", () => {
+//     immutableStore = immutableStore.produce((draft) => {
+//       draft[1].parent.name = "new name"
+//     })
+//   })
+// })
---
+++ libraries/aelastics-store/src/test-implementation/test.ts
@@ -0,0 +1,157 @@
+import { enablePatches, immerable, produce, produceWithPatches } from "immer"
+enablePatches()
+
+// The createClass function creates a class with a parent and child relation
+export function createClass(idMap: Map<string, any>) {
+  class Foo implements iFoo {
+    _id: string
+    _name: string
+    _parent: string | undefined = undefined
+    _child: string | undefined = undefined;
+    [immerable] = true
+
+    constructor(props: { id: string; name: string }) {
+      this._id = props.id
+      this._name = props.name
+    }
+
+    get id(): string {
+      return this._id
+    }
+
+    get name(): string {
+      return this._name
+    }
+
+    set name(value: string) {
+      this._name = value
+    }
+  }
+  const parent = "parent"
+  const child = "child"
+  const privateparent = "_parent"
+  const privatechild = "_child"
+
+  // Define the parent and child properties
+  Object.defineProperty(Foo.prototype, "parent", {
+    get(): Foo | undefined {
+      return this[privateparent] ? idMap.get(this[privateparent]) : undefined
+    },
+    set(value: Foo | undefined) {
+      // Disconnect the old inverse target
+      const oldvalue = this[parent]
+
+      if (oldvalue) {
+        oldvalue[privatechild] = undefined
+      }
+
+      if (value) {
+        value[privatechild] = this.id
+        this[privateparent] = value.id
+      } else {
+        this[privateparent] = undefined
+      }
+    },
+    enumerable: true,
+  })
+
+  Object.defineProperty(Foo.prototype, "child", {
+    get(): Foo | undefined {
+      return this[privatechild] ? idMap.get(this[privatechild]) : undefined
+    },
+    set(value: Foo | undefined) {
+      // Disconnect the old inverse target
+      const oldvalue = this[child]
+
+      if (oldvalue) {
+        oldvalue[privateparent] = undefined
+      }
+
+      if (value !== undefined) {
+        this[privatechild] = value.id
+        value[privateparent] = this.id
+      } else {
+        this[privatechild] = undefined
+      }
+    },
+    enumerable: true,
+  })
+
+  return Foo
+}
+
+// The interface for the Foo class
+export interface iFoo {
+  id: string
+  name: string
+  parent?: iFoo | undefined
+  child?: iFoo | undefined
+}
+
+// The TestStore class where only the state and/or the idMap is mutable
+export class TestStore {
+  private _state: any[]
+  private _idMap: Map<string, any>
+
+  constructor() {
+    this._state = []
+    this._idMap = new Map()
+  }
+
+  createObj(id: string, name: string) {
+    const obj = createClass(this._idMap)
+    const objInstance = new obj({ id, name })
+    this._idMap.set(id, objInstance)
+    return objInstance
+  }
+
+  produce(f: (draft: any) => void) {
+    const newState = produce(this._state, (draft) => f(draft))
+    this._state = newState
+  }
+
+  produceWithIdMap(f: (draft: any) => void) {
+    const [newState, patches] = produceWithPatches({ state: this._state, idMap: this._idMap }, (draft) =>
+      f(draft.state)
+    )
+    this._state = newState.state
+    this._idMap = newState.idMap
+  }
+
+  getState() {
+    return this._state
+  }
+}
+
+// The ImmutableTestStore class where the entire class is immutable
+export class ImmutableTestStore {
+  private _state: any[]
+  private _idMap: Map<string, any>;
+  [immerable] = true
+
+  constructor() {
+    this._state = []
+    this._idMap = new Map()
+  }
+
+  createObj(id: string, name: string) {
+    const obj = createClass(this._idMap)
+
+    const objInstance = new obj({ id, name })
+    // Produce an immutable object instance
+    const immutableObjInstance = produce(objInstance, (draft) => {})
+
+    // Store the immutable object instance in the map
+    this._idMap.set(id, immutableObjInstance)
+
+    return objInstance
+  }
+
+  produce(f: (draft: any) => void) {
+    return produce(this, (draft) => f(draft.getState()))
+  }
+
+  getState() {
+    return this._state
+  }
+}
