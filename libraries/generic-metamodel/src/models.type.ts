702a0ec5a1e122aa27fe2c90eff7d7bedb915029
Dejan Stojimirovic <stojimirovic.dejan@gmail.com>
2025-02-04 10:15:44 +0100

    fix schema name in models.type.ts. Add Import type. add new properties to Model type

--- libraries/generic-metamodel/src/models.type.ts
+++ libraries/generic-metamodel/src/models.type.ts
@@ -8,12 +8,12 @@ import { Diagram } from "./diagram.type";
 import { IDType, NameType, DescriptionType } from "./basic.type";
 import { RepositoryObject } from "./repository.type";
 
-export const ModelSchema = t.schema("RepModelSchema");
+export const ModelSchema = t.schema("ModelSchema");
 
 //TODO: change id type
 
 
-export const ModelElement = t.subtype(RepositoryObject,{
+export const ModelElement = t.subtype(RepositoryObject, {
     // name must be RegExp("^[a-zA-Z0-9_.-]+$")
     path: t.optional(t.string),
     // label is logically same as name, but can it be consisting of several words (and shown in multiple lines)
@@ -22,17 +22,26 @@ export const ModelElement = t.subtype(RepositoryObject,{
     content: t.string.derive('max_65535').maxLength(65535),
     parentModel: t.optional(t.link(ModelSchema, 'Model')), // to be defined as Ref
     namespace: t.optional(t.link(ModelSchema, 'Namespace')) // to be defined as Ref
-},  "ModelElement", ModelSchema);
+}, "ModelElement", ModelSchema);
 
 export const Namespace = t.subtype(ModelElement, {
     elements: t.arrayOf(ModelElement)
 }, "Namespace", ModelSchema);
+
+export const Import = t.object({
+    model: t.link(ModelSchema, 'Model'),
+    modelElements: t.arrayOf(ModelElement),
+    alias: t.optional(t.string) // must be unique whitin the import list
+}, "Import", ModelSchema);
 
 
 // TODO: add "status" property: Draft, Valid, Approved, Published, Archieved
 export const Model = t.subtype(Namespace, {
     MDA_level: t.optional(t.string.derive().oneOf(['M0', 'M1', 'M2', 'M3'])),
     diagrams: t.arrayOf(Diagram),
+    modelElements: t.arrayOf(ModelElement),
+    exportedElements: t.arrayOf(ModelElement),
+    imports: t.arrayOf(Import), // list of
 }, "Model", ModelSchema);
 
 // TODO: make parentModel and namespace to be reference type
