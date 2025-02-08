6787c97629fb904774808debfdfcdf45071e924d
Sinisa <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-02-08 18:23:04 +0100

    feat: Expand TypeSchema to include subclassing and optional properties
    - Added support for subclassing in TypeSchema with the 'extends' property.
    - Introduced the 'optional' property to indicate whether a property is optional.
    - Updated TypeMeta and PropertyMeta interfaces to reflect these changes.

--- libraries/eternal/src/handlers/MetaDefinitions.ts
+++ libraries/eternal/src/handlers/MetaDefinitions.ts
@@ -4,6 +4,7 @@ export type PropertyType = 'string' | 'number' | 'boolean' | 'object' | 'array' 
 /** Metadata for an individual property */
 export interface PropertyMeta {
     name: string; // Property name
+    optional?: boolean; // Whether the property is optional,
     type: PropertyType; // Data type of the property
     inverseType?: string; // Name of the inverse type (if bidirectional)
     inverseProp?: string; // Name of the inverse property (if bidirectional)
@@ -13,6 +14,7 @@ export interface PropertyMeta {
 export interface TypeMeta {
     name: string; // Name of the object type
     properties: Map<string, PropertyMeta>; // Property name -> PropertyMeta mapping
+    extends?: string; // Name of the base class (if subclassing is used)
 }
 
 /** Schema containing multiple type definitions */
