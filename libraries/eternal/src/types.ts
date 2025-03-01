cee18b153b3a63b5f3492b96104c29b3722f35dd
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-01 12:02:50 +0100

    feat: add ObservableExtra interface for enhanced observable creators

--- libraries/eternal/src/types.ts
+++ libraries/eternal/src/types.ts
@@ -1,3 +1,7 @@
+import { EternalStore } from "./EternalStore";
+import { EternalObject } from "./handlers/InternalTypes";
+import { PropertyMeta } from "./handlers/MetaDefinitions";
+
 export type UUID = string;
 
 export interface ChangeLog {
@@ -6,4 +10,12 @@ export interface ChangeLog {
     property?: string;
     oldValue?: any;
     newValue?: any;
+}
+
+
+// extra data for observable creators
+export interface ObservableExtra {
+    store: EternalStore;
+    object: EternalObject;
+    propDes: PropertyMeta;
 }
