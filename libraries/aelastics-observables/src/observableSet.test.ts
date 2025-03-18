c2c439b61d6733fc9cf52968ca0c96b456373636
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-18 21:30:21 +0100

    fix: refactor handlers in observableSet tests for improved clarity and consistency

--- libraries/aelastics-observables/src/observableSet.test.ts
+++ libraries/aelastics-observables/src/observableSet.test.ts
@@ -1,12 +1,47 @@
 import { createObservableSet, SetHandlers } from './observableSet';
 
+const handlers: Required<SetHandlers<number>> = {
+    add: jest.fn((target: Set<number>, value: number): [boolean, any] => {
+        return [true, target];
+    }),
+    delete: jest.fn((target: Set<number>, value: number): [boolean, boolean] => {
+        return [true, true];
+    }),
+    clear: jest.fn((target: Set<number>): [boolean, undefined] => {
+        return [true, undefined];
+    }),
+    has: jest.fn((target: Set<number>, value: number): [boolean, boolean] => {
+        return [true, target.has(value)];
+    }),
+    forEach: jest.fn((target: Set<number>, callback: (value: number, value2: number, set: Set<number>) => void): [boolean, any] => {
+        target.forEach(callback);
+        return [true, undefined];
+    }),
+    entries: jest.fn((target: Set<number>): [boolean, IterableIterator<[number, number]>] => {
+        return [true, target.entries()];
+    }),
+    keys: jest.fn((target: Set<number>): [boolean, IterableIterator<number>] => {
+        return [true, target.keys()];
+    }),
+    values: jest.fn((target: Set<number>): [boolean, IterableIterator<number>] => {
+        return [true, target.values()];
+    }),
+    size: jest.fn((target: Set<number>): [boolean, number] => {
+        return [true, target.size];
+    }),
+    toStringHandler: jest.fn((target: Set<number>): [boolean, string] => {
+        return [true, target.toString()];
+    }),
+
+    [Symbol.iterator]: jest.fn((target: Set<number>): IterableIterator<number> => {
+        return target[Symbol.iterator]();
+    }),
+
+};
+
 describe('createObservableSet', () => {
     it('should call the add handler', () => {
         const target = new Set<number>();
-        const handlers: SetHandlers<number> = {
-            add: jest.fn((set, value) => [false, set.add(value)]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         proxy.add(100);
 
@@ -16,10 +51,6 @@ describe('createObservableSet', () => {
 
     it('should call the delete handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            delete: jest.fn((set, value) => [false, set.delete(value)]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         proxy.delete(2);
 
@@ -29,13 +60,6 @@ describe('createObservableSet', () => {
 
     it('should call the clear handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            clear: jest.fn((set) => {
-                set.clear();
-                return [false, undefined];
-            }),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         proxy.clear();
 
@@ -45,10 +69,6 @@ describe('createObservableSet', () => {
 
     it('should call the has handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            has: jest.fn((set, value) => [false, set.has(value)]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const result = proxy.has(1);
 
@@ -58,10 +78,6 @@ describe('createObservableSet', () => {
 
     it('should call the forEach handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            forEach: jest.fn((set, callback) => [false, set.forEach(callback)]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         proxy.forEach((value) => {});
 
@@ -70,10 +86,6 @@ describe('createObservableSet', () => {
 
     it('should call the entries handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            entries: jest.fn((set) => [false, set.entries()]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const iterator = proxy.entries();
 
@@ -83,10 +95,6 @@ describe('createObservableSet', () => {
 
     it('should call the keys handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            keys: jest.fn((set) => [false, set.keys()]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const iterator = proxy.keys();
 
@@ -96,10 +104,6 @@ describe('createObservableSet', () => {
 
     it('should call the values handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            values: jest.fn((set) => [false, set.values()]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const iterator = proxy.values();
 
@@ -109,10 +113,6 @@ describe('createObservableSet', () => {
 
     it('should call the size handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            size: jest.fn((set) => [false, set.size]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const size = proxy.size;
 
@@ -122,10 +122,6 @@ describe('createObservableSet', () => {
 
     it('should call the Symbol.iterator handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            [Symbol.iterator]: jest.fn((set) => [false, set[Symbol.iterator]()]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const iterator = proxy[Symbol.iterator]();
 
@@ -135,14 +131,10 @@ describe('createObservableSet', () => {
 
     it('should call the toString handler', () => {
         const target = new Set<number>([1, 2, 3]);
-        const handlers: SetHandlers<number> = {
-            [Symbol.toStringTag]: jest.fn((set) => [false, set.toString()]),
-        };
-
         const proxy = createObservableSet(target, handlers, true);
         const result = proxy.toString();
 
-        expect(handlers.toString).toHaveBeenCalledWith(target);
+        expect(handlers.toStringHandler).toHaveBeenCalledWith(target);
         expect(result).toBe('[object Set]');
     });
 });
--- libraries/aelastics-observables/src/observableSet.ts
+++ libraries/aelastics-observables/src/observableSet.ts
@@ -1,87 +1,85 @@
 export interface SetHandlers<T> {
-    add?: (target: Set<T>, value: T) => [boolean, Set<T>];
-    delete?: (target: Set<T>, value: T) => [boolean, boolean];
-    clear?: (target: Set<T>) => [boolean, undefined];
-    has?: (target: Set<T>, value: T) => [boolean, boolean];
-    forEach?: (target: Set<T>, callback: (value: T, value2: T, set: Set<T>) => void) => [boolean, void];
-    entries?: (target: Set<T>) => [boolean, IterableIterator<[T, T]>];
-    keys?: (target: Set<T>) => [boolean, IterableIterator<T>];
-    values?: (target: Set<T>) => [boolean, IterableIterator<T>];
-    size?: (target: Set<T>) => [boolean, number];
-    [Symbol.iterator]?: (target: Set<T>) => [boolean, IterableIterator<T>];
-    [Symbol.toStringTag]?: (target: Set<T>) => [boolean, string];
+  add?: (target: Set<T>, value: T) => [boolean, Set<T>]
+  delete?: (target: Set<T>, value: T) => [boolean, boolean]
+  clear?: (target: Set<T>) => [boolean, undefined]
+  has?: (target: Set<T>, value: T) => [boolean, boolean]
+  forEach?: (target: Set<T>, callback: (value: T, value2: T, set: Set<T>) => void) => [boolean, void]
+  entries?: (target: Set<T>) => [boolean, IterableIterator<[T, T]>]
+  keys?: (target: Set<T>) => [boolean, IterableIterator<T>]
+  values?: (target: Set<T>) => [boolean, IterableIterator<T>]
+  size?: (target: Set<T>) => [boolean, number]
+  toStringHandler?: (target: Set<T>) => [boolean, string]
+  [Symbol.iterator]?: (target: Set<T>) => IterableIterator<T>
 }
 
-export function createObservableSet<T>(
-    set: Set<T>,
-    handlers: SetHandlers<T>,
-    defaultMutation: boolean = true,
-): Set<T> {
-    return new Proxy(set, {
-        get(target: Set<T>, key: string | number | symbol, receiver: any): any {
-            if (typeof key === 'string') {
-                switch (key) {
-                    case 'add':
-                        return (value: T) => {
-                            const [allowAdd, result] = handlers.add?.(target, value) ?? [defaultMutation, target];
-                            return allowAdd ? Reflect.apply(target.add, target, [value]) : result;
-                        };
-                    case 'delete':
-                        return (value: T) => {
-                            const [allowDelete, result] = handlers.delete?.(target, value) ?? [defaultMutation, false];
-                            return allowDelete ? Reflect.apply(target.delete, target, [value]) : result;
-                        };
-                    case 'clear':
-                        return () => {
-                            const [allowClear, result] = handlers.clear?.(target) ?? [defaultMutation, undefined];
-                            return allowClear ? Reflect.apply(target.clear, target, []) : result;
-                        };
-                    case 'has':
-                        return (value: T) => {
-                            const [allowHas, result] = handlers.has?.(target, value) ?? [defaultMutation, false];
-                            return allowHas ? Reflect.apply(target.has, target, [value]) : result;
-                        };
-                    case 'forEach':
-                        return (callback: (value: T, value2: T, set: Set<T>) => void) => {
-                            const [allowForEach, result] = handlers.forEach?.(target, callback) ?? [defaultMutation, undefined];
-                            return allowForEach ? Reflect.apply(target.forEach, target, [callback]) : result;
-                        };
-                    case 'entries':
-                        return () => {
-                            const [allowEntries, result] = handlers.entries?.(target) ?? [defaultMutation, target.entries()];
-                            return allowEntries ? Reflect.apply(target.entries, target, []) : result;
-                        };
-                    case 'keys':
-                        return () => {
-                            const [allowKeys, result] = handlers.keys?.(target) ?? [defaultMutation, target.keys()];
-                            return allowKeys ? Reflect.apply(target.keys, target, []) : result;
-                        };
-                    case 'values':
-                        return () => {
-                            const [allowValues, result] = handlers.values?.(target) ?? [defaultMutation, target.values()];
-                            return allowValues ? Reflect.apply(target.values, target, []) : result;
-                        };
-                    case 'size':
-                        const [proceed, result] = handlers.size?.(target) ?? [defaultMutation, undefined]
-                        return proceed ? target.size : result
-
-                }
+export function createObservableSet<T>(set: Set<T>, handlers: SetHandlers<T>, allow: boolean = true): Set<T> {
+  return new Proxy(set, {
+    get(target: Set<T>, key: string | number | symbol, receiver: any): any {
+      if (typeof key === "string") {
+        switch (key) {
+          case "add":
+            return (value: T) => {
+              const [allowAdd, result] = handlers.add?.(target, value) ?? [allow, target]
+              return allowAdd ? Reflect.apply(target.add, target, [value]) : result
             }
-
-            // Handle Symbol.iterator
-            if (key === Symbol.iterator) {
-                const [allowIterator, result] = handlers[Symbol.iterator]?.(target) ?? [defaultMutation, target[Symbol.iterator]()];
-                return allowIterator ? Reflect.apply(target[Symbol.iterator], target, []) : result;
+          case "delete":
+            return (value: T) => {
+              const [allowDelete, result] = handlers.delete?.(target, value) ?? [allow, false]
+              return allowDelete ? Reflect.apply(target.delete, target, [value]) : result
             }
-
-            // Handle Symbol.toStringTag
-            if (key === Symbol.toStringTag) {
-                const [allowToStringTag, result] = handlers[Symbol.toStringTag]?.(target) ?? [defaultMutation, 'Set'];
-                return allowToStringTag ? Reflect.get(target, Symbol.toStringTag) : result;
+          case "clear":
+            return () => {
+              const [allowClear, result] = handlers.clear?.(target) ?? [allow, undefined]
+              return allowClear ? Reflect.apply(target.clear, target, []) : result
             }
-
-            // Default behavior for other properties
-            console.warn(`Unhandled property ${key.toString()} in Set`);
+          case "has":
+            return (value: T) => {
+              const [allowHas, result] = handlers.has?.(target, value) ?? [allow, false]
+              return allowHas ? Reflect.apply(target.has, target, [value]) : result
+            }
+          case "forEach":
+            return (callback: (value: T, value2: T, set: Set<T>) => void) => {
+              const [allowForEach, result] = handlers.forEach?.(target, callback) ?? [allow, undefined]
+              return allowForEach ? Reflect.apply(target.forEach, target, [callback]) : result
+            }
+          case "entries":
+            return () => {
+              const [allowEntries, result] = handlers.entries?.(target) ?? [allow, target.entries()]
+              return allowEntries ? Reflect.apply(target.entries, target, []) : result
+            }
+          case "keys":
+            return () => {
+              const [allowKeys, result] = handlers.keys?.(target) ?? [allow, target.keys()]
+              return allowKeys ? Reflect.apply(target.keys, target, []) : result
+            }
+          case "values":
+            return () => {
+              const [allowValues, result] = handlers.values?.(target) ?? [allow, target.values()]
+              return allowValues ? Reflect.apply(target.values, target, []) : result
+            }
+          case "size":
+            const [proceedSize, resultSize] = handlers.size?.(target) ?? [allow, undefined]
+            return proceedSize ? target.size : resultSize
+          case "toString":
+            return () => {
+              const [proceedToString, resultToString] = handlers.toStringHandler?.(target) ?? [allow, undefined]
+              return proceedToString ? target.toString() : resultToString
+            }
         }
-    });
+      }
+
+      // Handle Symbol.iterator
+      if (key === Symbol.iterator) {
+        return () => {
+          const [continueOperation, result] = handlers[Symbol.iterator]?.(target) ?? [allow, undefined]
+          if (!continueOperation) {
+            return result
+          }
+          return Reflect.apply(Set.prototype[Symbol.iterator], target, receiver)
+        }
+      }
+      // Default behavior for other properties
+      console.warn(`Unhandled property ${key.toString()} in Set`)
+    },
+  })
 }
