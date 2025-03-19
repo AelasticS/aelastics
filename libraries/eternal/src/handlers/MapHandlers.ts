608e7ab204981fb77a39ea55950bc4b1919fb7f1
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-20 00:10:11 +0100

    fix: enhance map handlers to manage inverse relationships and improve structure

--- libraries/eternal/src/handlers/MapHandlers.ts
+++ libraries/eternal/src/handlers/MapHandlers.ts
@@ -1,13 +1,15 @@
-import { makePrivatePropertyKey} from "../utils"
+import { makePrivatePropertyKey, makeUpdateInverseKey } from "../utils"
 import { createObservableMap, MapHandlers } from "@aelastics/observables"
 import { ObservableExtra } from "../types"
 import { checkReadAccess, checkWriteAccess } from "../PropertyAccessors"
 import { EternalStore } from "../EternalStore"
 import { PropertyMeta } from "../meta/InternalSchema"
 
+import * as invUpd from "../inverseUpdaters"
+
 // Convert value UUID to Object
 const toValueObject = (item: any, store: EternalStore, propDes: PropertyMeta) =>
-    propDes.itemType === "object" && item ? store.getObject(item) : item
+  propDes.itemType === "object" && item ? store.getObject(item) : item
 
 // Convert key UUID to Object
 const toKeyObject = (item: any, store: EternalStore, propDes: PropertyMeta) =>
@@ -15,122 +17,144 @@ const toKeyObject = (item: any, store: EternalStore, propDes: PropertyMeta) =>
 
 // Convert value object to UUID if needed
 const valueToUUID = (value: any, propDes: PropertyMeta): any =>
-    propDes.itemType === "object" && value ? value.uuid : value
+  propDes.itemType === "object" && value ? value.uuid : value
 
 // Convert key object to UUID if needed
 const keyToUUID = (value: any, propDes: PropertyMeta): any =>
   propDes.keyType === "object" && value ? value.uuid : value
 
+export const createImmutableMapHandlers = <K, V>({ store, object, propDes }: ObservableExtra): MapHandlers<K, V> => {
+  const privateKey = makePrivatePropertyKey(propDes.qName)
+  const inverseUpdaterKey = propDes.inverseProp ? makeUpdateInverseKey(propDes.qName) : ""
 
-
-export const createImmutableMapHandlers = <K, V>({ store, object, propDes }: ObservableExtra): MapHandlers<K, V> => ({
-  /** Ensure values stored in the map are UUIDs if applicable */
-  set: (target: Map<K, V>, key: K, value: V) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const newValue = valueToUUID(value, propDes)
-    const newKey = keyToUUID(key, propDes)
-    const obj = checkWriteAccess(object, store, propDes.qName)
-    const res = obj[privateKey].set(newKey, newValue)
-    return [false, res]
-  },
-
-  /** Get value by key */
-  get: (target: Map<K, V>, key: K) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const obj = checkReadAccess(object, store)
-    const newKey = keyToUUID(key, propDes)
-    const newValue = obj[privateKey].get(newKey)
-    const res = toValueObject(newValue, store, propDes)
-    return [false, res]
-  },
-
-  /** Check if key exists */
-  has: (target: Map<K, V>, key: K) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const obj = checkReadAccess(object, store)
-    const newKey = keyToUUID(key, propDes)
-    return [false, obj[privateKey].has(newKey)]
-  },
-
-  /** Delete entry from map */
-  delete: (target: Map<K, V>, key: K) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const obj = checkReadAccess(object, store)
-    const newKey = keyToUUID(key, propDes)
-    const res = obj[privateKey].delete(newKey)
-    return [false, res]
-  },
-
-  /** Clear all entries from map */
-  clear: (target: Map<K, V>) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const obj = checkReadAccess(object, store)
-    const res = obj[privateKey].clear()
-    return [false, res]
-  },
-
-  /** Get size of map */
-  size: (target: Map<K, V>) => {
-    const privateKey = makePrivatePropertyKey(propDes.qName)
-    const obj = checkReadAccess(object, store)
-    const res = obj[privateKey].size
-    return [false, res]
-  },
-
-  /** Get keys iterator */
-  keys: (target: Map<K, V>) => {
-    const obj = checkReadAccess(object, store)
-
-    const key = makePrivatePropertyKey(propDes.qName)
-    const result = Array.from(obj[key].keys())
-      .map((k) => toKeyObject(k, store, propDes))
-      [Symbol.iterator]()
-    return [false, result]
-  },
-
-  /** Get values iterator */
-  values: (target: Map<K, V>) => {
-    const obj = checkReadAccess(object, store)
-    const key = makePrivatePropertyKey(propDes.qName)
-    const result = Array.from(obj[key].values())
-      .map((v) => toValueObject(v, store, propDes))
-      [Symbol.iterator]()
-    return [false, result]
-  },
-
-  /** Get entries iterator */
-  entries: (target: Map<K, V>) => {
-    const obj = checkReadAccess(object, store)
-    const key = makePrivatePropertyKey(propDes.qName)
-    const result = (<[[K, V]]>Array.from(obj[key].entries()))
-      .map(([k, v]) => [toKeyObject(k, store, propDes), toValueObject(v, store, propDes)] as [K, V])
-      [Symbol.iterator]()
-    return [false, result]
-  },
-
-  /** Execute function for each entry */
-  forEach: (target: Map<K, V>, callback: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any) => {
-    const obj = checkReadAccess(object, store)
-    const key = makePrivatePropertyKey(propDes.qName)
-    const result = obj[key].forEach((value: V, key: K, map: Map<K, V>) => {
-      const newKey = toKeyObject(key, store, propDes)
-      const newValue = toValueObject(value, store, propDes)
-      callback.call(thisArg, newValue, newKey, map)
-    })
-    return [false, result]
-  },
-
-  /** Handle Symbol.iterator */
-  [Symbol.iterator]: (target: Map<K, V>) => {
-    const obj = checkReadAccess(object, store)
-    const key = makePrivatePropertyKey(propDes.qName)
-    return (function* generator() {
-      for (const [k, v] of obj[key]) {
-        yield [toKeyObject(k, store, propDes), toValueObject(v, store, propDes)] as [K, V]
+  return {
+    /** Ensure values stored in the map are UUIDs if applicable */
+    set: (target: Map<K, V>, key: K, value: V) => {
+      const newValue = valueToUUID(value, propDes)
+      const newKey = keyToUUID(key, propDes)
+      const obj = checkWriteAccess(object, store, propDes.qName)
+      const res = obj[privateKey].set(newKey, newValue)
+      if (propDes.itemType === "object" && propDes.inverseProp) {
+        // set inverse to null
+        const updater: invUpd.inverseUpdater = obj[inverseUpdaterKey]
+        updater(obj, undefined, newValue)
       }
-    })()
-  },
-})
+      return [false, res]
+    },
+
+    /** Get value by key */
+    get: (target: Map<K, V>, key: K) => {
+      const obj = checkReadAccess(object, store)
+      const newKey = keyToUUID(key, propDes)
+      const newValue = obj[privateKey].get(newKey)
+      const res = toValueObject(newValue, store, propDes)
+      return [false, res]
+    },
+
+    /** Check if key exists */
+    has: (target: Map<K, V>, key: K) => {
+      const obj = checkReadAccess(object, store)
+      const keyUUID = keyToUUID(key, propDes)
+      return [false, obj[privateKey].has(keyUUID)]
+    },
+
+    /** Delete entry from map */
+    delete: (target: Map<K, V>, key: K) => {
+      const obj = checkReadAccess(object, store)
+      const keyUUID = keyToUUID(key, propDes)
+      if (propDes.itemType === "object" && propDes.inverseProp) {
+        // set inverse to null
+        const delObjUUID = obj[privateKey].get(keyUUID)
+        if (delObjUUID) {
+          const updater: invUpd.inverseUpdater = obj[inverseUpdaterKey]
+          updater(obj, delObjUUID, undefined)
+        }
+      }
+      const res = obj[privateKey].delete(keyUUID)
+      return [false, res]
+    },
+
+    /** Clear all entries from map */
+    clear: (target: Map<K, V>) => {
+      const privateKey = makePrivatePropertyKey(propDes.qName)
+      const obj = checkReadAccess(object, store)
+      if (propDes.itemType === "object" && propDes.inverseProp) {
+        const updater: invUpd.inverseUpdater = obj[inverseUpdaterKey]
+        // set inverse to null
+        for (const keyUUID of obj[privateKey].keys()) {
+          const delObjUUID = obj[privateKey].get(keyUUID)
+          if (delObjUUID) {
+            updater(obj, delObjUUID, undefined)
+          }
+        }
+      }
+      const res = obj[privateKey].clear()
+      return [false, res]
+    },
+
+    /** Get size of map */
+    size: (target: Map<K, V>) => {
+      const privateKey = makePrivatePropertyKey(propDes.qName)
+      const obj = checkReadAccess(object, store)
+      const res = obj[privateKey].size
+      return [false, res]
+    },
+
+    /** Get keys iterator */
+    keys: (target: Map<K, V>) => {
+      const obj = checkReadAccess(object, store)
+
+      const key = makePrivatePropertyKey(propDes.qName)
+      const result = Array.from(obj[key].keys())
+        .map((k) => toKeyObject(k, store, propDes))
+        [Symbol.iterator]()
+      return [false, result]
+    },
+
+    /** Get values iterator */
+    values: (target: Map<K, V>) => {
+      const obj = checkReadAccess(object, store)
+      const key = makePrivatePropertyKey(propDes.qName)
+      const result = Array.from(obj[key].values())
+        .map((v) => toValueObject(v, store, propDes))
+        [Symbol.iterator]()
+      return [false, result]
+    },
+
+    /** Get entries iterator */
+    entries: (target: Map<K, V>) => {
+      const obj = checkReadAccess(object, store)
+      const key = makePrivatePropertyKey(propDes.qName)
+      const result = (<[[K, V]]>Array.from(obj[key].entries()))
+        .map(([k, v]) => [toKeyObject(k, store, propDes), toValueObject(v, store, propDes)] as [K, V])
+        [Symbol.iterator]()
+      return [false, result]
+    },
+
+    /** Execute function for each entry */
+    forEach: (target: Map<K, V>, callback: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any) => {
+      const obj = checkReadAccess(object, store)
+      const key = makePrivatePropertyKey(propDes.qName)
+      const result = obj[key].forEach((value: V, key: K, map: Map<K, V>) => {
+        const newKey = toKeyObject(key, store, propDes)
+        const newValue = toValueObject(value, store, propDes)
+        callback.call(thisArg, newValue, newKey, map)
+      })
+      return [false, result]
+    },
+
+    /** Handle Symbol.iterator */
+    [Symbol.iterator]: (target: Map<K, V>) => {
+      const obj = checkReadAccess(object, store)
+      const key = makePrivatePropertyKey(propDes.qName)
+      return (function* generator() {
+        for (const [k, v] of obj[key]) {
+          yield [toKeyObject(k, store, propDes), toValueObject(v, store, propDes)] as [K, V]
+        }
+      })()
+    },
+  }
+}
 
 /** Helper function to create observable maps */
 export function createImmutableMap<K, V>(map: Map<K, V>, observableExtra: ObservableExtra): Map<K, V> {
