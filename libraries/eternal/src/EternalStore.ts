781dc0d8134ebefd58c9ed18a58213e754c61363
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-27 19:19:04 +0100

    fix/refactor: improve error handling in produce method and refactor undo method

--- libraries/eternal/src/EternalStore.ts
+++ libraries/eternal/src/EternalStore.ts
@@ -1,34 +1,37 @@
-import { ChangeLogEntry, consolidateChangeLogs, generateJsonPatch, hasChanges, JSONPatchOperation } from "./events/ChangeLog"
+import {
+  ChangeLogEntry,
+  consolidateChangeLogs,
+  generateJsonPatch,
+  hasChanges,
+  JSONPatchOperation,
+} from "./events/ChangeLog"
 import { addCopyPropsMethod, addPropertyAccessors } from "./PropertyAccessors"
 import { createImmutableArray } from "./handlers/ArrayHandlers"
 import { EternalClass, EternalObject } from "./handlers/InternalTypes"
-import { createImmutableMap} from "./handlers/MapHandlers"
-import {createImmutableSet} from "./handlers/SetHandlers"
+import { createImmutableMap } from "./handlers/MapHandlers"
+import { createImmutableSet } from "./handlers/SetHandlers"
 import { TypeMeta } from "./meta/InternalSchema"
 import { State } from "./State"
-import { SubscriptionManager } from "./events/SubscriptionManager";
-import { randomUUID } from 'crypto';
+import { SubscriptionManager } from "./events/SubscriptionManager"
+import { randomUUID } from "crypto"
 import { makePrivatePropertyKey, makePrivateProxyKey } from "./utils"
 
 export type InternalRecipe = ((obj: EternalObject) => void) | (() => any)
 
 export class EternalStore {
   private stateHistory: State[] = [] // Stores the history of states
-  private subscriptionManager = new SubscriptionManager(this); // Create a subscription manager
-  private currentStateIndex: number = -1// Track active state index
+  private subscriptionManager = new SubscriptionManager(this) // Create a subscription manager
+  private currentStateIndex: number = -1 // Track active state index
   private inUpdateMode: boolean = false // Flag to indicate if the store is in update mode
   private typeToClassMap: Map<string, any> = new Map() // Maps type names to dynamic classes
 
+  private accessedObjects: Set<EternalObject> = new Set() // Track accessed object
+  private versionedObjects: EternalObject[] = [] // Track versioned objects
 
-
-  private accessedObjects: Set<EternalObject> = new Set(); // Track accessed object
-  private versionedObjects: EternalObject[] = []; // Track versioned objects
-
-
-  private metaInfo: Map<string, TypeMeta>;
+  private metaInfo: Map<string, TypeMeta>
 
   constructor(metaInfo: Map<string, TypeMeta>) {
-    this.metaInfo = metaInfo;
+    this.metaInfo = metaInfo
     // Create dynamic classes for each type
     for (const [type, typeMeta] of metaInfo) {
       if (!this.typeToClassMap.has(type)) {
@@ -42,8 +45,8 @@ export class EternalStore {
   }
 
   public getSubscriptionManager(): SubscriptionManager {
-    return this.subscriptionManager;
-  }; 
+    return this.subscriptionManager
+  }
 
   /** Returns the latest (i.e. current) state */
   public getState(): State {
@@ -72,21 +75,24 @@ export class EternalStore {
     this.currentStateIndex++
   }
 
+  private revertToPreviousState(): boolean {
+    if (this.currentStateIndex >= 0) {
+      this.currentStateIndex--
+      return true // successful
+    }
+    return false // unsuccessful
+  }
   /** Undo the last change */
   public undo(): boolean {
-    if(this.inUpdateMode){
+    if (this.inUpdateMode) {
       throw new Error("Cannot undo while in update mode.")
     }
-    if (this.currentStateIndex > 0) {
-      this.currentStateIndex--
-      return true // Undo successful
-    }
-    return false // Cannot undo beyond initial state
+    return this.revertToPreviousState()
   }
 
   /** Redo the last undone change */
   public redo(): boolean {
-    if(this.inUpdateMode){
+    if (this.inUpdateMode) {
       throw new Error("Cannot redo while in update mode.")
     }
     if (this.currentStateIndex < this.stateHistory.length - 1) {
@@ -106,10 +112,10 @@ export class EternalStore {
     const DynamicClass = this.typeToClassMap.get(type)
     const newObject = new DynamicClass()
     // Generate unique values for each instance
-    newObject.uuid = randomUUID();
+    newObject.uuid = randomUUID()
 
     // Immediately add to the latest state
-    this.getState().addObject(newObject, 'created')
+    this.getState().addObject(newObject, "created")
 
     return newObject
   }
@@ -123,7 +129,7 @@ export class EternalStore {
   public fromState<T>(stateIndex: number, target: string | T): T | undefined {
     const state = this.getStateByIndex(stateIndex)
     if (!state) {
-      throw new Error(`State at index ${stateIndex} does not exist.`);
+      throw new Error(`State at index ${stateIndex} does not exist.`)
     }
     // If target is a UUID string
     if (typeof target === "string") {
@@ -140,11 +146,10 @@ export class EternalStore {
   /** Retrieves a specific historical state */
   public getStateByIndex(index: number): State {
     if (index < 0 || index >= this.stateHistory.length) {
-      throw new Error(`State at index ${index} does not exist.`);
+      throw new Error(`State at index ${index} does not exist.`)
     }
     return this.stateHistory[index]
   }
-
 
   /** Produces a new state with modifications */
   public produce<T extends object>(recipe: InternalRecipe, obj?: T): T | void {
@@ -152,48 +157,48 @@ export class EternalStore {
       throw new Error("Nested produce() calls are not allowed.")
     }
     this.inUpdateMode = true
-    this.accessedObjects.clear(); // Start tracking 
-    this.versionedObjects = []; // Reset list at start
+    this.accessedObjects.clear() // Start tracking
+    this.versionedObjects = [] // Reset list at start
     this.makeNewState()
     const currentState = this.getState()
 
-    if (obj) {  // Versioning logic when an object is passed
+    if (obj) {
+      // Versioning logic when an object is passed
       // Use State's method to create a new object version
       let newObj = obj as EternalObject
       try {
         recipe(newObj) // Apply modifications
         // Track changed objects
-        const additionalVersionedObjects = this.markVersionedObjects();
-        this.versionedObjects.push(...additionalVersionedObjects);
+        const additionalVersionedObjects = this.markVersionedObjects()
+        this.versionedObjects.push(...additionalVersionedObjects)
 
         if (this.versionedObjects.length > 0) {
-          this.subscriptionManager.notifyObjectSubscribers(/*this.versionedObjects*/); // Notify all updated objects
+          this.subscriptionManager.notifyObjectSubscribers(/*this.versionedObjects*/) // Notify all updated objects
 
           // If `obj` itself was versioned, return the latest version
-          const updatedObj = this.versionedObjects.find(o => o.uuid === newObj.uuid);
+          const updatedObj = this.versionedObjects.find((o) => o.uuid === newObj.uuid)
           if (updatedObj) {
-            newObj = updatedObj;
+            newObj = updatedObj
           }
         }
-      }
-      // TODO: handle errors
-
-      finally {
-        this.inUpdateMode = false
-        this.accessedObjects.clear(); // Stop tracking
-      }
-      return newObj as T
-    }
-    else {
-      try {
-        const result = (recipe as () => T)(); // Capture return value
-        this.markVersionedObjects();
-        this.subscriptionManager.notifyObjectSubscribers(/*this.versionedObjects*/); // Notify global changes
-        return result; // Return the result from recipe()
-
+      } catch (error) {
+        console.error("An error occurred while applying the recipe:", error)
+        this.revertToPreviousState()
+        throw error // Re-throw the error after logging it
       } finally {
         this.inUpdateMode = false
-        this.accessedObjects.clear(); // Stop tracking
+        this.accessedObjects.clear() // Stop tracking
+      }
+      return newObj as T
+    } else {
+      try {
+        const result = (recipe as () => T)() // Capture return value
+        this.markVersionedObjects()
+        this.subscriptionManager.notifyObjectSubscribers(/*this.versionedObjects*/) // Notify global changes
+        return result // Return the result from recipe()
+      } finally {
+        this.inUpdateMode = false
+        this.accessedObjects.clear() // Stop tracking
       }
     }
   }
@@ -201,102 +206,113 @@ export class EternalStore {
   // Track changed objects
   public trackVersionedObject(obj: EternalObject): void {
     if (!this.versionedObjects.includes(obj)) {
-      this.versionedObjects.push(obj);
+      this.versionedObjects.push(obj)
     }
   }
   private markVersionedObjects(): EternalObject[] {
-    const versionedObjects: EternalObject[] = [];
+    const versionedObjects: EternalObject[] = []
 
     for (const obj of this.accessedObjects) {
       if (hasChanges(this.getChangeLog(), obj["uuid"])) {
-        const newVersion = this.getState().createNewVersion(obj);
-        versionedObjects.push(newVersion);
+        const newVersion = this.getState().createNewVersion(obj)
+        versionedObjects.push(newVersion)
       }
     }
 
-    return versionedObjects;
+    return versionedObjects
   }
-
 
   public trackAccess(obj: EternalObject): void {
     if (this.inUpdateMode) {
-      this.accessedObjects.add(obj);
+      this.accessedObjects.add(obj)
     }
   }
 
-
   private createDynamicClass(typeMeta: TypeMeta, store: EternalStore) {
-    const className = typeMeta.qName; // Use the type name as the class name
-// TODO: support creation of subclasses recursively, so that order is not important
+    const className = typeMeta.qName // Use the type name as the class name
+    // TODO: support creation of subclasses recursively, so that order is not important
 
-    const superClass = typeMeta.extends ? this.getClassByName(typeMeta.extends) : undefined;
-    let BaseClass: any;
-    BaseClass = superClass ? superClass
-      : (typeMeta.extends ? this.createDynamicClass(this.metaInfo.get(typeMeta.extends)!, store) : EternalClass);
+    const superClass = typeMeta.extends ? this.getClassByName(typeMeta.extends) : undefined
+    let BaseClass: any
+    BaseClass = superClass
+      ? superClass
+      : typeMeta.extends
+      ? this.createDynamicClass(this.metaInfo.get(typeMeta.extends)!, store)
+      : EternalClass
     const DynamicClass = {
       [className]: class extends BaseClass {
         constructor() {
-          super(); // Call the constructor of the superclass
+          super() // Call the constructor of the superclass
           // Initialize properties based on type
           for (const [key, propertyMeta] of typeMeta.properties) {
-            const privateKey = makePrivatePropertyKey(key);
-            const proxyKey = makePrivateProxyKey(key);
+            const privateKey = makePrivatePropertyKey(key)
+            const proxyKey = makePrivateProxyKey(key)
             // Initialize the property based on type
             switch (propertyMeta.type) {
               case "array":
-              this[privateKey] = [];
-              this[proxyKey] = createImmutableArray(this[privateKey], { store, object: this as any, propDes: propertyMeta });
-              break;
+                this[privateKey] = []
+                this[proxyKey] = createImmutableArray(this[privateKey], {
+                  store,
+                  object: this as any,
+                  propDes: propertyMeta,
+                })
+                break
 
               case "map":
-              this[privateKey] = new Map();
-              this[proxyKey] = createImmutableMap(this[privateKey], { store, object: this as any, propDes: propertyMeta });
-              break;
+                this[privateKey] = new Map()
+                this[proxyKey] = createImmutableMap(this[privateKey], {
+                  store,
+                  object: this as any,
+                  propDes: propertyMeta,
+                })
+                break
 
               case "set":
-              this[privateKey] = new Set();
-              this[proxyKey] = createImmutableSet(this[privateKey], { store, object: this as any, propDes: propertyMeta });
-              break;
+                this[privateKey] = new Set()
+                this[proxyKey] = createImmutableSet(this[privateKey], {
+                  store,
+                  object: this as any,
+                  propDes: propertyMeta,
+                })
+                break
 
               case "string":
-              this[privateKey] = "";
-              break;
+                this[privateKey] = ""
+                break
 
               case "number":
-              this[privateKey] = 0;
-              break;
+                this[privateKey] = 0
+                break
 
               case "boolean":
-              this[privateKey] = false;
-              break;
+                this[privateKey] = false
+                break
 
               case "object":
-              this[privateKey] = undefined;
-              break;
+                this[privateKey] = undefined
+                break
 
               default:
-              this[privateKey] = undefined;
-              break;
+                this[privateKey] = undefined
+                break
             }
           }
         }
-      }
-    }[className];
+      },
+    }[className]
 
     // Property accessors (shared across instances) will handle access logic
-    addPropertyAccessors(DynamicClass.prototype, typeMeta, store);
-    // Add the copyProps method to the prototype  
-    addCopyPropsMethod(DynamicClass.prototype, typeMeta);
+    addPropertyAccessors(DynamicClass.prototype, typeMeta, store)
+    // Add the copyProps method to the prototype
+    addCopyPropsMethod(DynamicClass.prototype, typeMeta)
     // Store the class in the map
     this.typeToClassMap.set(className, DynamicClass)
-    return DynamicClass;
+    return DynamicClass
   }
-
-
 
   /** Returns the dynamic class for a given type name */
   public getClassByName(type: string): any {
-    return this.typeToClassMap.get(type);
+    return this.typeToClassMap.get(type)
   }
 
   public getChangeLog(): ChangeLogEntry[] {
