d4527a90807bdb122025dbe44e477629aca2911c
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-29 21:03:42 +0100

    refactor: remove unused useEternalSelector hook implementation

--- libraries/eternal/src/useEternalSelector.ts
+++
@@ -1,9 +0,0 @@
-// import { useSyncExternalStore } from "react";
-// import { Store } from "@aelastics/eternal";
-
-// export function useEternalSelector<T>(store: Store, selector: (state: Store) => T): T {
-//     return useSyncExternalStore(
-//         store.subscribe,  // Subscribe to store updates
-//         () => selector(store), // Get the selected state
-//     );
-// }
