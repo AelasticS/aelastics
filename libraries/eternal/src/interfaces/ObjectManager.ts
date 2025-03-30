52369d30fcaca2ac55dfb2a357d0142661426e9e
Sinisa Neskovic <45012244+Sinisa-Neskovic@users.noreply.github.com>
2025-03-30 04:28:13 +0200

    refactor: update ObjectManager interface

--- libraries/eternal/src/interfaces/ObjectManager.ts
+++ libraries/eternal/src/interfaces/ObjectManager.ts
@@ -1,5 +1,4 @@
-import { Result } from "../events/EventTypes";
-
+import { Result } from "../events/EventTypes"
 
 export interface ObjectManager {
   /**
@@ -7,7 +6,7 @@ export interface ObjectManager {
    * @param type - The name of object type defined in type schema.
    * @returns The newly created object.
    */
-  createObject<T extends object>(type: string, initialState?: T): T;
+  createObject<T extends object>(type: string, initialState?: T): T
 
   /**
    * Applies a recipe function to an object to update its state. A new state in store is automatically created
@@ -15,44 +14,44 @@ export interface ObjectManager {
    * @param obj - The object to be updated.
    * @returns The new version of the input object.
    */
-  update<T extends object>(recipe: (obj: T) => void, obj: T): T;
+  update<T extends object>(recipe: (obj: T) => void, obj: T): T
 
   /**
-   * Deletes an object from the store.
+   * Removes an object from the store.
    * @param obj - The object to be deleted.
    */
-  delete<T extends object>(obj: T): void;
+  delete<T extends object>(obj: T): void
 
   /**
    * Retrieves then latest version of an object by its UUID or reference.
-   * @param uuid - The UUID of the object to retrieve.
+   * @param obj - UUID or reference to an object within the store
    * @returns The object if found, otherwise undefined.
    */
-  getLastVersion<T extends object>(uuid: string): T | undefined;
+  getLastVersion<T extends object>(obj: string | T): T | undefined
   /**
    * Retrieves an object from a specific historical state.
    * @param stateIndex - The index of the historical state.
    * @param target - The target object or its identifier.
    * @returns The object from the specified historical state if found, otherwise undefined.
    */
-  fromState<T>(stateIndex: number, target: string | T): T | undefined;
+  fromState<T>(stateIndex: number, target: string | T): T | undefined
 
   /**
    * Converts a given object into an object under control of Store.
    */
-  makeImmutable<T>(obj: T): T;
+  immutable<T>(obj: T): T
 
   /**
    * Converts an object back to a regular (mutable) object, removing it from the control of Store.
    */
-  getMutable<T>(obj: T): T;
+  mutable<T>(obj: T): T
 
   /**
    * Serializes an object into a string format.
    * @param obj - The object to serialize.
    * @returns The serialized string representation of the object.
    */
-  serialize<T>(obj: T): string;
+  serialize<T>(obj: T): string
 
   /**
    * Deserializes a string into an object of type T and puts it into the store.
@@ -61,12 +60,12 @@ export interface ObjectManager {
    * @param validate - Optional flag to validate the object against its type schema. Default is true.
    * @returns The deserialized object of type T.
    */
-  deserialize<T>(obj: string, type: string, validate?: boolean): Result;
+  deserialize<T>(obj: string, type: string, validate?: boolean): T
 
   /**
    * Validates an object against its type schema.
    * @param obj - The object to validate.
    * @returns The result of the validation.
    */
-  validate<T>(obj: T): Result;
+  validate<T>(obj: T): Result
 }
